50364accf6c2cef24f34490a728e86fc29c264f4
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-09 15:52:05 -0500

    Basic slideshow component functionality is ready

--- components/elements/carousel.js
+++ components/elements/carousel.js
@@ -1,16 +1,128 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useEmblaCarousel } from "embla-carousel/react";
+import Markdown from "react-markdown";
+import Image from "next/image";
+import slide1 from "../../public/carousel1.jpeg";
+import slide2 from "../../public/carousel2.jpeg";
+
+const PrevButton = ({ enabled, onClick }) => (
+  <button
+    className="embla__button embla__button--prev"
+    onClick={onClick}
+    disabled={!enabled}
+    style={{ width: "20px" }}
+  >
+    <svg className="embla__button__svg" viewBox="137.718 -1.001 366.563 644">
+      <path d="M428.36 12.5c16.67-16.67 43.76-16.67 60.42 0 16.67 16.67 16.67 43.76 0 60.42L241.7 320c148.25 148.24 230.61 230.6 247.08 247.08 16.67 16.66 16.67 43.75 0 60.42-16.67 16.66-43.76 16.67-60.42 0-27.72-27.71-249.45-249.37-277.16-277.08a42.308 42.308 0 0 1-12.48-30.34c0-11.1 4.1-22.05 12.48-30.42C206.63 234.23 400.64 40.21 428.36 12.5z" />
+    </svg>
+  </button>
+);
+
+const NextButton = ({ enabled, onClick }) => (
+  <button
+    className="embla__button embla__button--next"
+    onClick={onClick}
+    disabled={!enabled}
+    style={{ width: "20px" }}
+  >
+    <svg className="embla__button__svg" viewBox="0 0 238.003 238.003">
+      <path d="M181.776 107.719L78.705 4.648c-6.198-6.198-16.273-6.198-22.47 0s-6.198 16.273 0 22.47l91.883 91.883-91.883 91.883c-6.198 6.198-6.198 16.273 0 22.47s16.273 6.198 22.47 0l103.071-103.039a15.741 15.741 0 0 0 4.64-11.283c0-4.13-1.526-8.199-4.64-11.313z" />
+    </svg>
+  </button>
+);
 
 export const EmblaCarousel = () => {
-  const [emblaRef] = useEmblaCarousel();
+  const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false });
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+
+  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
+  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setPrevBtnEnabled(embla.canScrollPrev());
+    setNextBtnEnabled(embla.canScrollNext());
+  }, [embla]);
+
+  useEffect(() => {
+    if (!embla) return;
+    embla.on("select", onSelect);
+    onSelect();
+  }, [embla, onSelect]);
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
+    <>
+      <div className="embla" ref={viewportRef}>
+        <div className="embla__container">
+          <div className="embla__slide">
+            <section className="container flex flex-col md:flex-row items-center justify-between py-12">
+              {/* Left column for content */}
+              <div className="flex-1 sm:pr-8">
+                {/* Hero section label */}
+                <p className="uppercase tracking-wide font-semibold">label1</p>
+                {/* Big title */}
+                <h1 className="title mt-2 sm:mt-0 mb-4 sm:mb-2">title1</h1>
+                {/* Description paragraph */}
+                <p className="text-xl mb-6">description11</p>
+                {/* Buttons row */}
+                {/* <div className="flex flex-row flex-wrap gap-4">
+    {data.buttons.map((button) => (
+      <ButtonLink
+        button={button}
+        appearance={getButtonAppearance(button.type, "light")}
+        key={button.id}
+      />
+    ))}
+  </div> */}
+                {/* Small rich text */}
+                <div className="text-base md:text-sm mt-4 sm:mt-3 rich-text-hero">
+                  <Markdown>small text1111</Markdown>
+                </div>
+              </div>
+              {/* Right column for the image */}
+              <div className="flex-shrink-0 w-full md:w-6/12 mt-6 md:mt-0">
+                <Image src={slide1} />
+              </div>
+            </section>
+          </div>
+          <div className="embla__slide">
+            {" "}
+            <section className="container flex flex-col md:flex-row items-center justify-between py-12">
+              {/* Left column for content */}
+              <div className="flex-1 sm:pr-8">
+                {/* Hero section label */}
+                <p className="uppercase tracking-wide font-semibold">label2</p>
+                {/* Big title */}
+                <h1 className="title mt-2 sm:mt-0 mb-4 sm:mb-2">title2</h1>
+                {/* Description paragraph */}
+                <p className="text-xl mb-6">description11</p>
+                {/* Buttons row */}
+                {/* <div className="flex flex-row flex-wrap gap-4">
+    {data.buttons.map((button) => (
+      <ButtonLink
+        button={button}
+        appearance={getButtonAppearance(button.type, "light")}
+        key={button.id}
+      />
+    ))}
+  </div> */}
+                {/* Small rich text */}
+                <div className="text-base md:text-sm mt-4 sm:mt-3 rich-text-hero">
+                  <Markdown>small text1111</Markdown>
+                </div>
+              </div>
+              {/* Right column for the image */}
+              <div className="flex-shrink-0 w-full md:w-6/12 mt-6 md:mt-0">
+                <Image src={slide2} />
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
-    </div>
+      <div>
+        <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
+        <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
+      </div>
+    </>
   );
 };
--- components/sections/feature-columns-group.js
+++ components/sections/feature-columns-group.js
@@ -1,22 +1,26 @@
-import NextImage from "../elements/image"
+import NextImage from "../elements/image";
+import { EmblaCarousel } from "../elements/carousel";
 
 const FeatureColumnsGroup = ({ data }) => {
   return (
-    <div
-      className="container flex flex-col lg:flex-row lg:flex-wrap gap-12 align-top py-12 text-white"
-      style={{ background: "#441d4f" }}
-    >
-      {data.features.map((feature) => (
-        <div className="flex-1 text-lg" key={feature.id}>
-          {/* <div className="w-10 h-10">
+    <>
+      <div
+        className="container flex flex-col lg:flex-row lg:flex-wrap gap-12 align-top py-12 text-white"
+        style={{ background: "#441d4f" }}
+      >
+        {data.features.map((feature) => (
+          <div className="flex-1 text-lg" key={feature.id}>
+            {/* <div className="w-10 h-10">
             <NextImage media={feature.icon} />
           </div> */}
-          <h3 className="font-bold mt-4 mb-4">{feature.title}</h3>
-          <p>{feature.description}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
+            <h3 className="font-bold mt-4 mb-4">{feature.title}</h3>
+            <p>{feature.description}</p>
+          </div>
+        ))}
+      </div>
+      <EmblaCarousel />
+    </>
+  );
+};
 
-export default FeatureColumnsGroup
+export default FeatureColumnsGroup;
