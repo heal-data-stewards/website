7d045afa7f470394296f95fb22517171fd41937c
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-13 04:49:56 -0500

    Deleetd unneccesary code

--- components/elements/data-table.js
+++ components/elements/data-table.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react"
-import { DataGrid } from "@material-ui/data-grid"
-import { getAllUsers } from "utils/api"
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { getAllUsers } from "utils/api";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -21,32 +21,32 @@ const columns = [
     headerName: "Organization",
     width: 150,
   },
-]
+];
 function createData(id, firstname, lastname, username, email, org) {
-  return { id, firstname, lastname, username, email, org }
+  return { id, firstname, lastname, username, email, org };
 }
 
 export default function DataTable() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   // Call the strapi API to GET all users
   useEffect(() => {
     getAllUsers()
       .then(function (result) {
-        return result.map((user, x) => {
+        return result.map((user, index) => {
           return createData(
-            x,
+            index,
             user.firstname,
             user.lastname,
             user.username,
             user.email,
             user.organization
-          )
-        })
+          );
+        });
       })
       .then((result) => {
-        setUsers(result)
-      })
-  }, [])
+        setUsers(result);
+      });
+  }, []);
   return (
     <div
       style={{ height: 400, width: "100%" }}
@@ -60,5 +60,5 @@ export default function DataTable() {
         checkboxSelection
       />
     </div>
-  )
+  );
 }
