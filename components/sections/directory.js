c381ecb594a7a221f1bf1c319a039d19ba206d5c
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-18 15:50:03 -0500

    Directory is only accessible to authorized users

--- components/sections/directory.js
+++ components/sections/directory.js
@@ -1,11 +1,28 @@
-import React from "react"
-
-import DataTable from "../elements/data-table"
+import React, { useState, useEffect } from "react";
+import { useSession, getSession } from "next-auth/client";
+import DataTable from "../elements/data-table";
 
 export default function Directory() {
+  const [session, loading] = useSession();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (session) {
+      setLoggedIn(true);
+    }
+  });
+
   return (
     <>
-      <DataTable />
+      {loggedIn && <DataTable />}
+      {!loggedIn && (
+        <div className="container">
+          <br></br>
+          <br></br>
+          <p>Access Denied</p>
+          <p>Please Log In</p>
+        </div>
+      )}
     </>
-  )
+  );
 }
