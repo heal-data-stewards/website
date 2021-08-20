127ab8b28f67ac4e838a97839bc227e9725eea46
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-20 00:07:55 -0500

    linted for deployment

--- components/elements/carousel.js
+++ components/elements/carousel.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { useEmblaCarousel } from "embla-carousel/react";
-import Markdown from "react-markdown";
-import Image from "next/image";
-import slide1 from "../../public/carousel1.jpeg";
-import slide2 from "../../public/carousel2.jpeg";
-import Paper from "@material-ui/core/Paper";
-import slide3 from "../../public/slide3.jpeg";
+import React, { useState, useEffect, useCallback } from "react"
+import { useEmblaCarousel } from "embla-carousel/react"
+import Markdown from "react-markdown"
+import Image from "next/image"
+import slide1 from "../../public/carousel1.jpeg"
+import slide2 from "../../public/carousel2.jpeg"
+import Paper from "@material-ui/core/Paper"
+import slide3 from "../../public/slide3.jpeg"
 
 const PrevButton = ({ enabled, onClick }) => (
   <button
@@ -18,7 +18,7 @@ const PrevButton = ({ enabled, onClick }) => (
       <path d="M428.36 12.5c16.67-16.67 43.76-16.67 60.42 0 16.67 16.67 16.67 43.76 0 60.42L241.7 320c148.25 148.24 230.61 230.6 247.08 247.08 16.67 16.66 16.67 43.75 0 60.42-16.67 16.66-43.76 16.67-60.42 0-27.72-27.71-249.45-249.37-277.16-277.08a42.308 42.308 0 0 1-12.48-30.34c0-11.1 4.1-22.05 12.48-30.42C206.63 234.23 400.64 40.21 428.36 12.5z" />
     </svg>
   </button>
-);
+)
 
 const NextButton = ({ enabled, onClick }) => (
   <button
@@ -31,26 +31,26 @@ const NextButton = ({ enabled, onClick }) => (
       <path d="M181.776 107.719L78.705 4.648c-6.198-6.198-16.273-6.198-22.47 0s-6.198 16.273 0 22.47l91.883 91.883-91.883 91.883c-6.198 6.198-6.198 16.273 0 22.47s16.273 6.198 22.47 0l103.071-103.039a15.741 15.741 0 0 0 4.64-11.283c0-4.13-1.526-8.199-4.64-11.313z" />
     </svg>
   </button>
-);
+)
 
 export const EmblaCarousel = () => {
-  const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false });
-  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
-  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+  const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false })
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false)
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false)
 
-  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
-  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla])
+  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla])
   const onSelect = useCallback(() => {
-    if (!embla) return;
-    setPrevBtnEnabled(embla.canScrollPrev());
-    setNextBtnEnabled(embla.canScrollNext());
-  }, [embla]);
+    if (!embla) return
+    setPrevBtnEnabled(embla.canScrollPrev())
+    setNextBtnEnabled(embla.canScrollNext())
+  }, [embla])
 
   useEffect(() => {
-    if (!embla) return;
-    embla.on("select", onSelect);
-    onSelect();
-  }, [embla, onSelect]);
+    if (!embla) return
+    embla.on("select", onSelect)
+    onSelect()
+  }, [embla, onSelect])
 
   return (
     <div className="embla" ref={viewportRef} style={{ color: "#373a3c" }}>
@@ -95,7 +95,7 @@ export const EmblaCarousel = () => {
             </div>
             {/* Right column for the image */}
             <div className="flex-shrink-0 w-full md:w-6/12 mt-6 md:mt-0 img-fix">
-              <Image src={slide1} className="card-2" alt="img" />
+              <Image src={slide1} alt="img" />
             </div>
           </section>
         </div>
@@ -183,20 +183,10 @@ export const EmblaCarousel = () => {
                   REGISTER HERE.
                 </a>
               </p>
-              {/* Buttons row */}
-              {/* <div className="flex flex-row flex-wrap gap-4">
-    {data.buttons.map((button) => (
-      <ButtonLink
-        button={button}
-        appearance={getButtonAppearance(button.type, "light")}
-        key={button.id}
-      />
-    ))}
-  </div> */}
             </div>
             {/* Right column for the image */}
             <div className="flex-shrink-0 w-full md:w-6/12 mt-6 md:mt-0 img-fix">
-              <Image src={slide3} className="card-2" alt="img" />
+              <Image src={slide3} alt="img" />
             </div>
           </section>
         </div>
@@ -206,5 +196,5 @@ export const EmblaCarousel = () => {
         <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
       </div>
     </div>
-  );
-};
+  )
+}
