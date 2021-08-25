ccf63da489bc53314a5f6fda9bc6b37af807f784
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-25 12:20:45 -0700

    Reverted changes to the nextauth page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import Adapters from "next-auth/adapters"
-import axios from "axios"
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import axios from "axios";
 
 const options = {
   providers: [
@@ -19,28 +18,23 @@ const options = {
               identifier: credentials.email,
               password: credentials.password,
             }
-          )
+          );
           if (data) {
             // console.log(data);
-            return data
+            return data;
           } else {
-            return null
+            return null;
           }
         } catch (e) {
           // console.log('caught error');
           // const errorMessage = e.response.data.message
           // Redirecting to the login page with error message          in the URL
           // throw new Error(errorMessage + '&email=' + credentials.email)
-          return null
+          return null;
         }
       },
     }),
   ],
-  adapter: Adapters.TypeORM.Adapter({
-    type: "postgresql", // or mysql, postgresql, mssql
-    database: ":memory:",
-    synchronize: false,
-  }),
 
   session: {
     jwt: true,
@@ -52,32 +46,32 @@ const options = {
   callbacks: {
     // Getting the JWT token from API response
     jwt: async (token, user, account) => {
-      const isSignIn = user ? true : false
+      const isSignIn = user ? true : false;
       if (isSignIn) {
-        token.jwt = user.jwt
-        token.id = user.user.id
-        token.name = user.user.username
-        token.email = user.user.email
-        token.firstname = user.user.firstname
-        token.lastname = user.user.lastname
-        token.organization = user.user.organization
+        token.jwt = user.jwt;
+        token.id = user.user.id;
+        token.name = user.user.username;
+        token.email = user.user.email;
+        token.firstname = user.user.firstname;
+        token.lastname = user.user.lastname;
+        token.organization = user.user.organization;
         // token.picture = user.user.picture.url;
       }
-      return Promise.resolve(token)
+      return Promise.resolve(token);
     },
 
     session: async (session, user) => {
-      session.jwt = user.jwt
-      session.id = user.id
-      session.firstname = user.firstname
-      session.lastname = user.lastname
-      session.organization = user.organization
+      session.jwt = user.jwt;
+      session.id = user.id;
+      session.firstname = user.firstname;
+      session.lastname = user.lastname;
+      session.organization = user.organization;
       // session.picture = user.picture;
-      return Promise.resolve(session)
+      return Promise.resolve(session);
     },
   },
-}
+};
 
-const Deliver = (req, res) => NextAuth(req, res, options)
+const Deliver = (req, res) => NextAuth(req, res, options);
 
-export default Deliver
+export default Deliver;
