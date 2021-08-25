69caaba9396066f94f50f001e67d1a8149e00841
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-25 09:31:38 -0700

    Profile editing is now done through the strapi user permissions plugin

--- components/elements/form/account-edit.js
+++ components/elements/form/account-edit.js
@@ -1,47 +1,53 @@
-import { useState } from "react"
-import { fetchAPI } from "utils/api"
-import * as yup from "yup"
-import { Formik, Form, Field } from "formik"
-import Button from "../button"
-import React from "react"
-import { TextField } from "formik-material-ui"
+import { useState } from "react";
+import { fetchAPI } from "utils/api";
+import * as yup from "yup";
+import { Formik, Form, Field } from "formik";
+import Button from "../button";
+import React from "react";
+import { TextField } from "formik-material-ui";
+const axios = require("axios");
+import { signIn, useSession } from "next-auth/client";
 
 const EditForm = ({ data }) => {
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false);
+  const [session, loading] = useSession();
 
   const LeadSchema = yup.object().shape({
-    email: yup.string().email(),
-    name: yup.string().required(),
-    inquiry: yup.string().required(),
-  })
+    firstname: yup.string(),
+    lastname: yup.string(),
+  });
 
   return (
     <div className="py-10 text-center">
       <h1 className="text-3xl mb-10 font-bold mb-2">Edit Form</h1>
       <div className="flex flex-col items-center">
         <Formik
-          initialValues={{ email: "", name: "", inquiry: "" }}
+          initialValues={{ firstname: "", lastname: "" }}
           validationSchema={LeadSchema}
           onSubmit={async (values, { setSubmitting, setErrors }) => {
-            setLoading(true)
-
+            // setLoading(true);
+            console.log(session);
             try {
-              setErrors({ api: null })
-              await fetchAPI("/lead-form-submissions", {
-                method: "POST",
-                body: JSON.stringify({
-                  email: values.email,
-                  name: values.name,
-                  inquiry: values.inquiry,
-                  location: data.location,
-                }),
-              })
+              const path = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/users/${session.id}`;
+              setErrors({ api: null });
+              const res = await axios.put(
+                path,
+                {
+                  firstname: values.firstname,
+                  lastname: values.lastname,
+                },
+                {
+                  headers: {
+                    Authorization: `Bearer ${session.jwt}`,
+                  },
+                }
+              );
             } catch (err) {
-              setErrors({ api: err.message })
+              setErrors({ api: err.message });
             }
-
-            setLoading(false)
-            setSubmitting(false)
+            signIn("email", session.user.email);
+            // setLoading(false)
+            // setSubmitting(false)
           }}
         >
           {({ errors, touched, isSubmitting }) => (
@@ -49,48 +55,33 @@ const EditForm = ({ data }) => {
               <Form className="flex flex-wrap flex-col md:flex-row gap-4">
                 <Field
                   className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
-                  type="firstname"
+                  type="text"
                   name="firstname"
                   placeholder={"First Name"}
                   component={TextField}
-                />{" "}
+                />
                 <Field
                   className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
-                  type="lastname"
+                  type="text"
                   name="lastname"
                   placeholder={"Last Name"}
                   component={TextField}
                 />
-                <Field
-                  className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
-                  // type="email"
-                  name="email"
-                  placeholder={"Email"}
-                  component={TextField}
-                />
-                <Field
-                  className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
-                  type="organization"
-                  name="organization"
-                  placeholder={"Organization"}
-                  component={TextField}
-                />
-                <Button
+                <button
                   type="submit"
-                  button={{ text: "submit" }}
-                  disabled={isSubmitting}
-                  loading={loading}
-                />
+
+                  // disabled={isSubmitting}
+                  // loading={loading}
+                >
+                  submit
+                </button>
               </Form>
-              {/* <p className="text-red-500 h-12 text-sm mt-1 ml-2 text-left">
-                {(errors.email && touched.email && errors.email) || errors.api}
-              </p> */}
             </div>
           )}
         </Formik>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditForm
+export default EditForm;
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import axios from "axios"
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import axios from "axios";
 
 const options = {
   providers: [
@@ -18,18 +18,19 @@ const options = {
               identifier: credentials.email,
               password: credentials.password,
             }
-          )
+          );
           if (data) {
-            return data
+            // console.log(data);
+            return data;
           } else {
-            return null
+            return null;
           }
         } catch (e) {
           // console.log('caught error');
           // const errorMessage = e.response.data.message
           // Redirecting to the login page with error message          in the URL
           // throw new Error(errorMessage + '&email=' + credentials.email)
-          return null
+          return null;
         }
       },
     }),
@@ -45,30 +46,30 @@ const options = {
   callbacks: {
     // Getting the JWT token from API response
     jwt: async (token, user, account) => {
-      const isSignIn = user ? true : false
+      const isSignIn = user ? true : false;
       if (isSignIn) {
-        token.jwt = user.jwt
-        token.id = user.user.id
-        token.name = user.user.username
-        token.email = user.user.email
-        token.firstname = user.user.firstname
-        token.lastname = user.user.lastname
-        token.organization = user.user.organization
+        token.jwt = user.jwt;
+        token.id = user.user.id;
+        token.name = user.user.username;
+        token.email = user.user.email;
+        token.firstname = user.user.firstname;
+        token.lastname = user.user.lastname;
+        token.organization = user.user.organization;
         // token.picture = user.user.picture.url;
       }
-      return Promise.resolve(token)
+      return Promise.resolve(token);
     },
 
     session: async (session, user) => {
-      session.jwt = user.jwt
-      session.id = user.id
-      session.firstname = user.firstname
-      session.lastname = user.lastname
-      session.organization = user.organization
+      session.jwt = user.jwt;
+      session.id = user.id;
+      session.firstname = user.firstname;
+      session.lastname = user.lastname;
+      session.organization = user.organization;
       // session.picture = user.picture;
-      return Promise.resolve(session)
+      return Promise.resolve(session);
     },
   },
-}
+};
 
-export default (req, res) => NextAuth(req, res, options)
+export default (req, res) => NextAuth(req, res, options);
