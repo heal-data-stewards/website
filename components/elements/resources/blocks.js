e8cd3b3f93d7c9c50dadb199be12700972f88ea1
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-09-23 03:49:52 -0500

    Added a page title to the resources page

--- components/elements/resources/blocks.js
+++ components/elements/resources/blocks.js
@@ -3,15 +3,15 @@ import Link from 'next/link'
 
 export default function Block({ data }) {
     // console.log(data)
-    return (<Link href="/" >
+    return (<Link href={`/resources/${data.url || "coming-soon"}`} >
         <div className="resource-block" style={{
             position: "relative", backgroundImage: `url(${data.img.url})`, clipPath: "polygon(0 0, 100% 0, 100% 80%, 80% 100%, 0 100%)", backgroundSize: "cover",
-            cursor: "pointer", 
+            cursor: "pointer",
             height: "400px",
             marginBottom: "2.5rem"
         }}>
             <h1 className="text-white text-4xl p-10 resource-block-title" style={{
-                backgroundColor: "transparent", 
+                backgroundColor: "transparent",
                 position: "absolute",
                 zIndex: "2",
                 wordBreak: "break-word"
