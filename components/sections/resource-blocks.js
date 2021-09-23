a9df9dc34b792ff4dec9c3ecf6429789fb0999d8
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-09-23 03:49:52 -0500

    Added a page title to the resources page

--- components/sections/resource-blocks.js
+++ components/sections/resource-blocks.js
@@ -1,14 +1,26 @@
-import React from "react"
-import { Block } from "../elements/resources"
+import React from "react";
+import { Block } from "../elements/resources";
 
-export default function ResourceBlocks(data ) {
-    const { resourceblocks } = data.data
+import Divider from "@mui/material/Divider";
 
-    return (
-        <div className="container flex flex-wrap justify-between">
-            {resourceblocks.map((block,i)=>{
-                return <Block data={block} key={block + i}/>
-            })}
-        </div>
-    )
+export default function ResourceBlocks(data) {
+  const { resourceblocks } = data.data;
+  return (
+    <div className="container">
+      <section className="pt-10 pb-10">
+        <h1 className="text-5xl font-black pb-4 text-purple">Resources</h1>
+        <Divider />
+        <p className="text-xl text-gray pt-4">
+          The HEAL Data Stewardship Group (HEAL Stewards) is committed to
+          providing a variety of documents to assist HEAL Investigators in
+          navigating and contributing to the HEAL data ecosystem.
+        </p>
+      </section>
+      <div className="flex flex-wrap justify-between">
+        {resourceblocks.map((block, i) => {
+          return <Block data={block} key={block + i} />;
+        })}
+      </div>
+    </div>
+  );
 }
