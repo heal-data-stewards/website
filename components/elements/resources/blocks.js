e953933044a9998ac5c9b92e61f60dd14e410943
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-09-23 03:09:11 -0500

    Resource blocks are now mobile friendly

--- components/elements/resources/blocks.js
+++ components/elements/resources/blocks.js
@@ -6,13 +6,15 @@ export default function Block({ data }) {
     return (<Link href="/" >
         <div className="resource-block" style={{
             position: "relative", backgroundImage: `url(${data.img.url})`, clipPath: "polygon(0 0, 100% 0, 100% 80%, 80% 100%, 0 100%)", backgroundSize: "cover",
-            cursor: "pointer", height: "400px",
-            width: "380px",
+            cursor: "pointer", 
+            height: "400px",
             marginBottom: "2.5rem"
         }}>
             <h1 className="text-white text-4xl p-10 resource-block-title" style={{
-                backgroundColor: "transparent", position: "absolute",
-                zIndex: "2"
+                backgroundColor: "transparent", 
+                position: "absolute",
+                zIndex: "2",
+                wordBreak: "break-word"
             }} >{data.title}</h1>
         </div></Link>
     )
