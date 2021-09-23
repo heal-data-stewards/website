081b0c9c18b2de60dec33c93ff633841f23eadd0
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-09-22 22:39:05 -0500

    purple resource boxes now have a darker purple on hover

--- components/sections/resource-blocks.js
+++ components/sections/resource-blocks.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Block } from "../elements/resources"
 
-export default function ResourceBlocks({ data }) {
+export default function ResourceBlocks(data ) {
+    const { resourceblocks } = data.data
+
     return (
-        <div className="container">
-            <p>hi</p>
+        <div className="container flex flex-wrap justify-between">
+            {resourceblocks.map((block,i)=>{
+                return <Block data={block} key={block + i}/>
+            })}
         </div>
     )
 }
