b3ccc50e61bcd506f1c92ba3229b9e3e6ff60d1a
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-09-23 22:56:53 -0500

    created a new page title component and removed the hard coded title from the resources page, it will now be added automatically when configuered on the backend

--- components/sections/resource-blocks.js
+++ components/sections/resource-blocks.js
@@ -1,21 +1,11 @@
 import React from "react";
 import { Block } from "../elements/resources";
 
-import Divider from "@mui/material/Divider";
-
 export default function ResourceBlocks(data) {
   const { resourceblocks } = data.data;
+  // console.log(data)
   return (
     <div className="container">
-      <section className="pt-10 pb-10">
-        <h1 className="text-5xl font-black pb-4 text-purple">Resources</h1>
-        <Divider />
-        <p className="text-xl text-gray pt-4">
-          The HEAL Data Stewardship Group (HEAL Stewards) is committed to
-          providing a variety of documents to assist HEAL Investigators in
-          navigating and contributing to the HEAL data ecosystem.
-        </p>
-      </section>
       <div className="flex flex-wrap justify-between">
         {resourceblocks.map((block, i) => {
           return <Block data={block} key={block + i} />;
