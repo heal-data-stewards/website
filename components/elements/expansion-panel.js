c765ef941d8b2688b3470c48cc4bc24cb23a3375
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-09-30 05:58:35 -0700

    Renamed the mui expansion panel to accordion

--- components/elements/expansion-panel.js
+++ components/elements/expansion-panel.js
@@ -1,11 +1,9 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { withStyles } from "@material-ui/core/styles"
-import ExpansionPanel from "@material-ui/core/ExpansionPanel"
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary"
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails"
-import Typography from "@material-ui/core/Typography"
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core";
 
 const styles = (theme) => ({
   root: {
@@ -15,31 +13,31 @@ const styles = (theme) => ({
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
   },
-})
+});
 
 function SimpleExpansionPanel({ classes, data }) {
   return (
     <div className={classes.root}>
       {data.map((faq, i) => {
         return (
-          <ExpansionPanel key={faq.question + i}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Accordion key={faq.question + i}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
               <Typography className={classes.heading}>
                 {faq.question}
               </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
+            </AccordionSummary>
+            <AccordionDetails>
               <Typography>{faq.answer}</Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        )
+            </AccordionDetails>
+          </Accordion>
+        );
       })}
     </div>
-  )
+  );
 }
 
 SimpleExpansionPanel.propTypes = {
   classes: PropTypes.object.isRequired,
-}
+};
 
-export default withStyles(styles)(SimpleExpansionPanel)
+export default withStyles(styles)(SimpleExpansionPanel);
