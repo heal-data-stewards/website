d076de832410c6526d9f22867ec7d92d3da67ffd
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-13 07:21:06 -0500

    working group tables now filter by working group type configuered through strapi

--- components/sections/table/list-item.js
+++ components/sections/table/list-item.js
@@ -5,7 +5,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 
 export default function WorkingGroupListItem({name, organization}) {
   return (
-    <ListItem sx={{ width: "33.3%", padding: "8px 0"}} >
+    <ListItem sx={{ width: "33.3%", padding: "10px 0"}} >
       <ListItemButton>
         <ListItemText primary={name} sx={{ color: "#532565"}} />
         <ListItemText secondary={organization} sx={{ color: "#532565"}} />
