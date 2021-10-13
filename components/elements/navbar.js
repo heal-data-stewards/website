01203879d46ad10a97eeed3aca878665ce901880
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-13 11:13:47 -0500

    Added a profile button on the top right hand corner

--- components/elements/navbar.js
+++ components/elements/navbar.js
@@ -1,30 +1,35 @@
-import React, { useState, useEffect } from "react"
-import PropTypes from "prop-types"
-import Link from "next/link"
-import { useRouter } from "next/router"
-import Image from "next/image"
-import { getButtonAppearance } from "utils/button"
-import { mediaPropTypes, linkPropTypes, buttonLinkPropTypes } from "utils/types"
-import { MdMenu } from "react-icons/md"
-import ButtonLink from "./button-link"
-import NextImage from "./image"
-import CustomLink from "./custom-link"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import Drawer from "@material-ui/core/Drawer"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemText from "@material-ui/core/ListItemText"
-import Divider from "@material-ui/core/Divider"
-import { signIn, signOut, useSession, getSession } from "next-auth/client"
-import Button from "@material-ui/core/Button"
-import Fade from "@material-ui/core/Fade"
-import { Btn, Btn2 } from "../elements/button"
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { getButtonAppearance } from "utils/button";
+import {
+  mediaPropTypes,
+  linkPropTypes,
+  buttonLinkPropTypes,
+} from "utils/types";
+import { MdMenu } from "react-icons/md";
+import ButtonLink from "./button-link";
+import NextImage from "./image";
+import CustomLink from "./custom-link";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { signIn, signOut, useSession, getSession } from "next-auth/client";
+import Button from "@material-ui/core/Button";
+import Fade from "@material-ui/core/Fade";
+import { Btn, Btn2 } from "../elements/button";
+import AccountMenu from "./profile";
 
 const Navbar = ({ navbar, pageContext }) => {
-  const [session, loading] = useSession()
-  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false)
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [session, loading] = useSession();
+  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [navigationItems, setNavigationItems] = useState([
     {
       id: 27,
@@ -38,11 +43,11 @@ const Navbar = ({ navbar, pageContext }) => {
       newTab: false,
       text: "CALENDAR",
     },
-  ])
+  ]);
 
   useEffect(() => {
     if (session || loggedIn) {
-      setNavigationItems(navbar.links)
+      setNavigationItems(navbar.links);
     } else {
       setNavigationItems([
         {
@@ -57,15 +62,15 @@ const Navbar = ({ navbar, pageContext }) => {
           newTab: false,
           text: "CALENDAR",
         },
-      ])
+      ]);
     }
-  }, [session, navbar.links, loggedIn])
-  const router = useRouter()
+  }, [session, navbar.links, loggedIn]);
+  const router = useRouter();
   const handleLogOut = () => {
-    signOut({ redirect: false })
-  }
+    signOut({ redirect: false });
+  };
 
-  if (loading) return null
+  if (loading) return null;
 
   return (
     <>
@@ -114,13 +119,12 @@ const Navbar = ({ navbar, pageContext }) => {
                   </>
                 )}
                 {session && (
-                  <>
-                    <Btn
-                      handleClick={handleLogOut}
-                      button={{ text: "Log Out" }}
+                  <div>
+                    <AccountMenu
+                      handleLogOut={handleLogOut}
                       setLoggedIn={setLoggedIn}
                     />
-                  </>
+                  </div>
                 )}
               </div>
             )}
@@ -197,8 +201,8 @@ const Navbar = ({ navbar, pageContext }) => {
         </>
       )}
     </>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   navbar: PropTypes.shape({
@@ -210,6 +214,6 @@ Navbar.propTypes = {
     button: buttonLinkPropTypes,
   }),
   initialLocale: PropTypes.string,
-}
+};
 
-export default Navbar
+export default Navbar;
