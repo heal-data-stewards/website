e44a2b438b74d8eea4d24729a603604fc1a389b8
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-13 07:21:06 -0500

    working group tables now filter by working group type configuered through strapi

--- components/sections/table/table.js
+++ components/sections/table/table.js
@@ -1,26 +1,31 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react"
 import List from "@mui/material/List";
 import WorkingGroupListItem from "./list-item";
-
-const dummyData = [
-  { name: "Sergio Rocha", organization: "renci" },
-  { name: "Sergio Rocha", organization: "renci"  },
-  { name: "Sergio Rocha", organization: "renci"  },
-  { name: "Sergio Rocha", organization: "renci"  },
-  { name: "Sergio Rocha", organization: "renci"  },
-];
+import { getAllUsers } from "utils/api"
 
 export default function WorkingGroupTable({ data }) {
+  const [users, setUsers] = useState([])
   console.log(data);
+    // Call the strapi API to GET all users
+    useEffect(() => {
+      getAllUsers()
+        .then(function (result) {
+          const users = result.filter((user) => user.organization === data.title.workinggrouptype)
+          setUsers(users)
+        })
+    }, [])
   return (
     <div className="container">
+      <section style={{boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)", margin: "0 0 15px 0"}}>
+        <h1 className="text-3xl text-purple text-center p-3" style={{background: "#e5e0e7"}}>{data.title.title}</h1>
+      </section>
       <nav aria-label="working group list">
         <List className="flex flex-wrap">
-          {dummyData.map((member, index) => {
+          {users.map((member, index) => {
             return (
               <WorkingGroupListItem
-                key={member.name + index}
-                name={member.name}
+                key={member.firstname + index}
+                name={member.firstname + " " + member.lastname}
                 organization={member.organization}
               />
             );
