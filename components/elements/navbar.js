f9c1fe4744bae73220aadd03738b15869f71ed09
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-13 11:18:11 -0500

    Removed unnessecary code

--- components/elements/navbar.js
+++ components/elements/navbar.js
@@ -1,35 +1,27 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import { getButtonAppearance } from "utils/button";
-import {
-  mediaPropTypes,
-  linkPropTypes,
-  buttonLinkPropTypes,
-} from "utils/types";
-import { MdMenu } from "react-icons/md";
-import ButtonLink from "./button-link";
-import NextImage from "./image";
-import CustomLink from "./custom-link";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
-import { signIn, signOut, useSession, getSession } from "next-auth/client";
-import Button from "@material-ui/core/Button";
-import Fade from "@material-ui/core/Fade";
-import { Btn, Btn2 } from "../elements/button";
-import AccountMenu from "./profile";
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import Image from "next/image"
+import { mediaPropTypes, linkPropTypes, buttonLinkPropTypes } from "utils/types"
+import { MdMenu } from "react-icons/md"
+import NextImage from "./image"
+import CustomLink from "./custom-link"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Drawer from "@material-ui/core/Drawer"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import Divider from "@material-ui/core/Divider"
+import { signIn, signOut, useSession, getSession } from "next-auth/client"
+import { Btn2 } from "../elements/button"
+import AccountMenu from "./profile"
 
 const Navbar = ({ navbar, pageContext }) => {
-  const [session, loading] = useSession();
-  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [session, loading] = useSession()
+  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false)
   const [navigationItems, setNavigationItems] = useState([
     {
       id: 27,
@@ -43,11 +35,11 @@ const Navbar = ({ navbar, pageContext }) => {
       newTab: false,
       text: "CALENDAR",
     },
-  ]);
+  ])
 
   useEffect(() => {
     if (session || loggedIn) {
-      setNavigationItems(navbar.links);
+      setNavigationItems(navbar.links)
     } else {
       setNavigationItems([
         {
@@ -62,15 +54,15 @@ const Navbar = ({ navbar, pageContext }) => {
           newTab: false,
           text: "CALENDAR",
         },
-      ]);
+      ])
     }
-  }, [session, navbar.links, loggedIn]);
-  const router = useRouter();
+  }, [session, navbar.links, loggedIn])
+  const router = useRouter()
   const handleLogOut = () => {
-    signOut({ redirect: false });
-  };
+    signOut({ redirect: false })
+  }
 
-  if (loading) return null;
+  if (loading) return null
 
   return (
     <>
@@ -186,9 +178,8 @@ const Navbar = ({ navbar, pageContext }) => {
                     )}
                     {session && (
                       <>
-                        <Btn
-                          handleClick={handleLogOut}
-                          button={{ text: "Log Out" }}
+                        <AccountMenu
+                          handleLogOut={handleLogOut}
                           setLoggedIn={setLoggedIn}
                         />
                       </>
@@ -201,8 +192,8 @@ const Navbar = ({ navbar, pageContext }) => {
         </>
       )}
     </>
-  );
-};
+  )
+}
 
 Navbar.propTypes = {
   navbar: PropTypes.shape({
@@ -214,6 +205,6 @@ Navbar.propTypes = {
     button: buttonLinkPropTypes,
   }),
   initialLocale: PropTypes.string,
-};
+}
 
-export default Navbar;
+export default Navbar
--- components/elements/profile.js
+++ components/elements/profile.js
@@ -1,32 +1,37 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import * as React from "react"
+import Box from "@mui/material/Box"
+import Avatar from "@mui/material/Avatar"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import Divider from "@mui/material/Divider"
+import IconButton from "@mui/material/IconButton"
+import Tooltip from "@mui/material/Tooltip"
+import Logout from "@mui/icons-material/Logout"
+import Link from "next/link"
 
-export default function AccountMenu({handleLogOut, setLoggedIn}) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+export default function AccountMenu({ handleLogOut, setLoggedIn }) {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
   return (
     <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', margin: "0 10px 0 0" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+          margin: "0 10px 0 0",
+        }}
+      >
         <Tooltip title="Account settings">
           <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
-            <Avatar sx={{ width: 42, height: 42, background: "#532565" }}/>
+            <Avatar sx={{ width: 42, height: 42, background: "#532565" }} />
           </IconButton>
         </Tooltip>
       </Box>
@@ -38,44 +43,44 @@ export default function AccountMenu({handleLogOut, setLoggedIn}) {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         <MenuItem>
-          <Avatar /> 
-          <a href="/account">My account</a>
+          <Avatar />
+          <Link href="/account">My account</Link>
         </MenuItem>
         <Divider />
         <MenuItem>
           <ListItemIcon>
             <Logout fontSize="small" />
           </ListItemIcon>
-          <a onClick={handleLogOut}>Log Out</a>
+          <Link onClick={handleLogOut}>Log Out</Link>
         </MenuItem>
       </Menu>
     </React.Fragment>
-  );
+  )
 }
