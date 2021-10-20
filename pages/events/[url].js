7b21eaa4c2fc2afba08cc259fbe3e988667e7aa3
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-20 17:44:16 -0500

    now pulling events from the calendar and injecting event data into the event pages

--- pages/events/[url].js
+++ pages/events/[url].js
@@ -1,7 +1,7 @@
-import Layout from "@/components/layout"
-import { getPageData, getGlobalData } from "utils/api"
-import Seo from "@/components/elements/seo"
-import { getAuthorizationToken } from "utils/msft-graph-api"
+import Layout from "@/components/layout";
+import { getPageData, getGlobalData } from "utils/api";
+import Seo from "@/components/elements/seo";
+import { getAuthorizationToken } from "utils/msft-graph-api";
 
 function Eventpage({ global, event, pageContext, metadata }) {
   // Render event page...
@@ -16,46 +16,49 @@ function Eventpage({ global, event, pageContext, metadata }) {
         {/* Page header section */}
         <section>
           <h1 className="text-5xl font-black pb-8 text-gray-dark">
-            {event.url}
+            {event.event.subject}
           </h1>
           <p className="text-xl text-gray-dark">{event.url}</p>
         </section>
       </div>
     </Layout>
-  )
+  );
 }
 
 // This function gets called at build time
 export async function getStaticPaths() {
-  // Call an external API endpoint to get posts, the outlook endpoint here
-  getAuthorizationToken()
-  const dummyEndpoints = [{ url: "event1" }, { url: "event2" }]
+  // Call an external API endpoint to get posts
+  const events = await getAuthorizationToken();
+
+  const eventPaths = events.map((event)=>{
+    return {url: event.id}
+  })
 
   // Get the paths we want to pre-render based on posts
-  const paths = dummyEndpoints.map((event) => ({
-    params: { url: event.url },
-  }))
+  const paths = eventPaths.map((event) => ({
+    params: { url: event.url},
+  }));
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
 // This also gets called at build time
 export async function getStaticProps(context) {
-  const { locale, locales, defaultLocale, preview = null } = context
+  const { locale, locales, defaultLocale, preview = null } = context;
   // params contains the event `url`.
   // If the route is like /event/1, then params.event is 1
   // const res = await fetch(`https://.../events/${params.url}`)
-  // const event = await res.json()
+  const eventData = await getAuthorizationToken(context.params.url);
 
   // Get the navbar and footer from strapi
-  const globalLocale = await getGlobalData(locale)
-  const params = { slug: ["calendar"] }
-  const pageData = await getPageData({ slug: params.slug }, locale, preview)
+  const globalLocale = await getGlobalData(locale);
+  const params = { slug: ["calendar"] };
+  const pageData = await getPageData({ slug: params.slug }, locale, preview);
 
   // We have the required page data, pass it to the page component
-  const { metadata, localizations, slug } = pageData
+  const { metadata, localizations, slug } = pageData;
 
   const pageContext = {
     locale: pageData.locale,
@@ -63,9 +66,9 @@ export async function getStaticProps(context) {
     defaultLocale,
     slug,
     localizations,
-  }
+  };
 
-  const event = { url: context.params.url, title: "event title example" }
+  const event = { url: context.params.url, event: eventData };
 
   // Pass post data to the page via props
   return {
@@ -77,7 +80,7 @@ export async function getStaticProps(context) {
         ...pageContext,
       },
     },
-  }
+  };
 }
 
-export default Eventpage
+export default Eventpage;
--- utils/msft-graph-api.js
+++ utils/msft-graph-api.js
@@ -1,42 +1,75 @@
 import axios from "axios";
 
 function getEvents(token) {
-  console.log(token)
-  axios
-    .get(`https://graph.microsoft.com/v1.0/users`, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => {
-      console.log(res.data.value);
-    })
-    .catch((error) => {
-      console.log(error.response.data);
-    });
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        `https://graph.microsoft.com/v1.0/users/${process.env.USER_ID}/calendar/events`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then((res) => {
+        resolve(res.data.value);
+      })
+      .catch((error) => {
+        reject(error.response.data);
+      });
+  });
 }
 
-export function getAuthorizationToken() {
+function getEvent(token,id) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        `https://graph.microsoft.com/v1.0/users/${process.env.USER_ID}/calendar/events/${id}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((error) => {
+        reject(error.response.data);
+      });
+  });
+}
+
+export async function getAuthorizationToken(id) {
   const params = new URLSearchParams();
   const headers = {
     "Content-Type": "application/x-www-form-urlencoded",
   };
-
+  let event = "";
   params.append("client_id", process.env.CLIENT_ID);
   params.append("scope", process.env.SCOPE);
   params.append("client_secret", process.env.CLIENT_SECRET);
   params.append("grant_type", process.env.GRANT_TYPE);
 
-  axios
+  const token = await axios
     .post(
       `https://login.microsoftonline.com/${process.env.MSFT_TENANT}/oauth2/v2.0/token`,
       params,
       headers
     )
     .then((res) => {
-      getEvents(res.data.access_token);
+      return res.data.access_token;
     })
     .catch((error) => {
-      console.log(error);
+      return error;
     });
+
+    if (id === undefined) {
+      event = await getEvents(token);
+    } else {
+      event = await getEvent(token, id);
+      console.log(event)
+    }
+
+  return event;
 }
