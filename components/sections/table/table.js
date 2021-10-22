32cc6cfea2932829e0965a6df2c45e1b30f98954
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-22 13:54:44 -0500

    Added an emails button to the working groups

--- components/sections/table/table.js
+++ components/sections/table/table.js
@@ -1,22 +1,42 @@
-import React, { useEffect, useState } from "react"
-import List from "@mui/material/List"
-import WorkingGroupListItem from "./list-item"
-import { getAllUsers } from "utils/api"
+import React, { useEffect, useState } from "react";
+import List from "@mui/material/List";
+import WorkingGroupListItem from "./list-item";
+import { getAllUsers } from "utils/api";
+import Button from "@mui/material/Button";
 
 export default function WorkingGroupTable({ data }) {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   // Call the strapi API to GET all users
   useEffect(() => {
     getAllUsers().then(function (result) {
       const users = result.filter(
         (user) => user.workgroup === data.title.workinggroup
-      )
-      setUsers(users)
+      );
+      setUsers(users);
+    });
+  }, [data.title.workinggroup]);
+
+  const mailTo = users
+    .map((user) => {
+      return user.username + ";";
     })
-  }, [data.title.workinggroup])
+    .join();
 
   return (
     <div className="container">
+      <section
+        style={{
+          boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+          margin: "0 0 55px 0",
+        }}
+      >
+        <h1
+          className="text-3xl text-purple text-center p-3"
+          style={{ background: "#e5e0e7" }}
+        >
+          Chair
+        </h1>
+      </section>
       <section
         style={{
           boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
@@ -39,59 +59,33 @@ export default function WorkingGroupTable({ data }) {
                 name={member.firstname + " " + member.lastname}
                 organization={member.organization}
               />
-            )
+            );
           })}
         </List>
       </nav>
+      <section className="mb-8 mt-8">
+        <p className="pr-3 pb-3">
+          To copy a list of email addresses for all collaboration group members,
+          right click the 'EMAILS' button and select 'Copy Email Address' from
+          the menu.
+        </p>
+        <Button variant="contained" href={`mailto:${mailTo}`}>
+          Emails
+        </Button>
+      </section>
+      <section
+        style={{
+          boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+          margin: "0 0 55px 0",
+        }}
+      >
+        <h1
+          className="text-3xl text-purple text-center p-3"
+          style={{ background: "#e5e0e7" }}
+        >
+          Resources
+        </h1>
+      </section>
     </div>
-  )
+  );
 }
-
-// import React, { useEffect, useState } from "react"
-// import List from "@mui/material/List";
-// import WorkingGroupListItem from "./list-item";
-// import { getAllUsers } from "utils/api"
-
-// export default function WorkingGroupTable({ data }) {
-//   const [users, setUsers] = useState([])
-//   // console.log(data);
-//     // Call the strapi API to GET all users
-//     useEffect(() => {
-//       getAllUsers()
-//         .then(function (result) {
-//           const users = result.filter((user) => {
-//             const regExSearch = "/"+ data.title.workinggrouptype.toLowerCase() + "/g"
-
-//             if (user.organization !== null) {
-//               const lowerCaseOrganization = user.organization.toLowerCase()
-//               console.log(lowerCaseOrganization)
-//               console.log(regExSearch)
-//             }
-
-//             // return user.organization.match(test)
-//           })
-//           setUsers(users)
-//         })
-//     }, [])
-
-//   return (
-//     <div className="container">
-//       <section style={{boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)", margin: "0 0 15px 0"}}>
-//         <h1 className="text-3xl text-purple text-center p-3" style={{background: "#e5e0e7"}}>{data.title.title}</h1>
-//       </section>
-//       <nav aria-label="working group list">
-//         <List className="flex flex-wrap">
-//           {users.map((member, index) => {
-//             return (
-//               <WorkingGroupListItem
-//                 key={member.firstname + index}
-//                 name={member.firstname + " " + member.lastname}
-//                 organization={member.organization}
-//               />
-//             );
-//           })}
-//         </List>
-//       </nav>
-//     </div>
-//   );
-// }
