bc37beb7f2f79723207bbd02299d9d6df0234a16
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-22 12:18:58 -0500

    Calendar now filters events for non signed in users

--- components/sections/calendar.js
+++ components/sections/calendar.js
@@ -1,35 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { useSession, getSession } from "next-auth/client";
-import DataTable from "../elements/data-table";
-import { hastToReact } from "react-markdown/src/ast-to-react";
+import { useSession } from "next-auth/client";
 import BasicCard from "../elements/card";
 
 export default function Calendar({ data, eventData }) {
-  // const [events, setEvents] = useState([{}]);
-  // const [session, loading] = useSession()
-  // const [loggedIn, setLoggedIn] = useState(false)
+  const [events, setEvents] = useState([]);
+  const [session, loading] = useSession();
+  const [loggedIn, setLoggedIn] = useState(false);
 
-
+  useEffect(() => {
+    if (session) {
+      setLoggedIn(true);
+      // eventData contains every event in the HEAL calendar, logged in users see every event
+      setEvents(eventData);
+    } else {
+      // Events created in the HEAL Calendar created with out a category label are collected in filteredEvents
+      // These are the events avaiable to the public
+      const filteredEvents = eventData.filter((event, i) => {
+        return event.categories.length === 0;
+      });
+      setEvents(filteredEvents);
+    }
+  }, [session]);
 
   return (
     <>
-      {/* {loggedIn && <DataTable />} */}
-      {/* {!loggedIn && (
+      {loggedIn && (
         <div className="container">
-          <br></br>
-          <br></br>
-          <p>Access Denied</p>
-          <p>Please Log In</p>
+          {/* Logged In Events List */}
+          <section>
+            {events.map((event, i) => {
+              return <BasicCard key={event.subject + i} event={event} />;
+            })}
+          </section>
         </div>
-      )}       */}
-      <div className="container">
-        {/* Events List */}
-        <section>
-          {eventData.map((event,i) => {
-            return <BasicCard key={event.subject+i} event={event} />;
-          })}
-        </section>
-      </div>
+      )}
+      {!loggedIn && (
+        <div className="container">
+          {/* Public Events List */}
+          <section>
+            {events.length !== 0 &&
+              events.map((event, i) => {
+                return <BasicCard key={event.subject + i} event={event} />;
+              })}
+          </section>
+        </div>
+      )}
     </>
   );
 }
