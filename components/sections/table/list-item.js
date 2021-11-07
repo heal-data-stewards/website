7db798cbd273515a0b33a1cd80e2056daca497d3
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-11-06 22:59:12 -0500

    Added more styling to the working group list items

--- components/sections/table/list-item.js
+++ components/sections/table/list-item.js
@@ -1,12 +1,23 @@
-import * as React from "react";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import Chip from "@mui/material/Chip";
+import * as React from "react"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import Chip from "@mui/material/Chip"
+import Image from "next/image"
 
 export default function WorkingGroupListItem({ name, organization }) {
   return (
-    <ListItem sx={{ width: "50%", padding: "10px 0" }}>
-      <ListItemButton>
+    <ListItem className="working-group-list-item" >
+      {/* <ListItemButton> */}
+      <div className="">
+          <Image
+            alt="alt"
+            width={75}
+            height={75}
+            src={
+              "https://heal-community-portal-api.s3.amazonaws.com/blank_profile_picture_973460_1280_a29a12e75d.png"
+            }
+          />
+        </div>
         <h2
           style={{
             color: "#982568",
@@ -18,7 +29,7 @@ export default function WorkingGroupListItem({ name, organization }) {
           {name}
         </h2>
         <Chip label={organization} />
-      </ListItemButton>
+      {/* </ListItemButton> */}
     </ListItem>
-  );
+  )
 }
