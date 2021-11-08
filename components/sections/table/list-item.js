03e99772e8798ee6ac2343d57503e9edc69e4125
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-11-08 12:04:06 -0600

    ran lint fix

--- components/sections/table/list-item.js
+++ components/sections/table/list-item.js
@@ -6,29 +6,29 @@ import Image from "next/image"
 
 export default function WorkingGroupListItem({ name, organization }) {
   return (
-    <ListItem className="working-group-list-item" >
+    <ListItem className="working-group-list-item">
       {/* <ListItemButton> */}
       <div className="">
-          <Image
-            alt="alt"
-            width={75}
-            height={75}
-            src={
-              "https://heal-community-portal-api.s3.amazonaws.com/blank_profile_picture_973460_1280_a29a12e75d.png"
-            }
-          />
-        </div>
-        <h2
-          style={{
-            color: "#982568",
-            padding: "0 16px",
-            textTransform: "capitalize",
-            fontWeight: "600",
-          }}
-        >
-          {name}
-        </h2>
-        <Chip label={organization} />
+        <Image
+          alt="alt"
+          width={75}
+          height={75}
+          src={
+            "https://heal-community-portal-api.s3.amazonaws.com/blank_profile_picture_973460_1280_a29a12e75d.png"
+          }
+        />
+      </div>
+      <h2
+        style={{
+          color: "#982568",
+          padding: "0 16px",
+          textTransform: "capitalize",
+          fontWeight: "600",
+        }}
+      >
+        {name}
+      </h2>
+      <Chip label={organization} />
       {/* </ListItemButton> */}
     </ListItem>
   )
