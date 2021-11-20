5a370c892bc7423a894552c73e6233ad4fcd2949
Sergio Rocha <rochascript@gmail.com>
2021-11-20 08:43:40 -0600

    Making a call to the outllok api on event rendering to get up to date outlook changes

--- next.config.js
+++ next.config.js
@@ -6,14 +6,5 @@ module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: 'secretheal',
-    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: '/static',
-  },
+  }
 };
