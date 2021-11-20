f3255507e2a719b0bad4c80fe6fb3b6bb9983bac
Sergio Rocha <rochascript@gmail.com>
2021-11-19 20:33:52 -0600

    Fixed bug in glossary where the text styling would change on search, and added a page title directly

--- components/sections/glossary.js
+++ components/sections/glossary.js
@@ -1,13 +1,8 @@
-import React, { useState } from "react"
-import Paper from "@material-ui/core/Paper"
-import { makeStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
-import SearchBar from "../elements/search-bar"
-import Divider from "@mui/material/Divider"
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchBar from "../elements/search-bar";
+import Divider from "@mui/material/Divider";
+import PageTitle from "./page-title";
 
 const useStyles = makeStyles({
   root: {
@@ -24,20 +19,20 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-})
+});
 
 const filterByValue = (array, string) => {
   return array.filter((o) =>
     Object.keys(o).some((k) =>
       String(o[k]).toLowerCase().includes(string.toLowerCase())
     )
-  )
-}
+  );
+};
 
 export default function Glossary({ data }) {
-  const [filter, setFilter] = React.useState("")
-  const classes = useStyles()
-  let items
+  const [filter, setFilter] = React.useState("");
+  const classes = useStyles();
+  let items;
   if (filter === "") {
     items = data.term.map((word, i) => {
       return (
@@ -55,41 +50,37 @@ export default function Glossary({ data }) {
             </h3>
           </div>
         </>
-      )
-    })
+      );
+    });
   } else {
     items = filterByValue(data.term, filter).map((word, i) => {
       return (
         <>
-          <div className={classes.root} key={word + i}>
-            <Typography
-              variant="h5"
-              component="h2"
-              className="text-magenta capitalize"
-              style={{ fontWeight: "600" }}
+          <div className={classes.root + " mt-10"} key={word + i}>
+            <h2
+              className="text-magenta capitalize pb-2"
+              style={{ fontWeight: "600", fontSize: "1.2rem" }}
             >
               {word.title}
-            </Typography>
-            <Typography
-              className={classes.pos}
-              color="textSecondary"
-            ></Typography>
-            <Typography variant="body2" component="p">
+            </h2>
+            <Divider />
+            <h3 className="pt-4" style={{ fontWeight: "100" }}>
               {word.body}
-              {/* <br />
-                        {'"a benevolent smile"'} */}
-            </Typography>
+            </h3>
           </div>
         </>
-      )
-    })
+      );
+    });
   }
   return (
-    <main className="container pb-12 ">
-      <div>
-        <SearchBar setFilter={setFilter} filter={filter} />
-        {items}
-      </div>
-    </main>
-  )
+    <>
+      <PageTitle data={{ title: data.title }}  noPaddingBottom="true" />
+      <main className="container pb-12 ">
+        <div>
+          <SearchBar setFilter={setFilter} filter={filter} />
+          {items}
+        </div>
+      </main>
+    </>
+  );
 }
