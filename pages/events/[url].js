0c77ecc853a2af5ef418bdb59b31f412a4be8bde
Sergio Rocha <rochascript@gmail.com>
2021-11-20 03:34:28 -0600

    Switched back and trying ISR

--- pages/events/[url].js
+++ pages/events/[url].js
@@ -13,7 +13,7 @@ function Eventpage({ global, event, pageContext, metadata }) {
   // TO DO: Remove everything other than the body
   // console.log(event.event.body.content);
   const date = new Date(Date.parse(event.event.start.dateTime))
-  console.log(event.event.body.content)
+  // console.log(event.event.body.content)
   return (
     <Layout global={global} pageContext={pageContext}>
       <Seo metadata={metadata} />
@@ -53,27 +53,66 @@ function Eventpage({ global, event, pageContext, metadata }) {
   )
 }
 
-// // This function gets called at build time
-// export async function getStaticPaths() {
-//   // Call an external API endpoint to get posts
-//   const events = await getAuthorizationToken()
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const events = await getAuthorizationToken()
 
-//   const eventPaths = events.map((event) => {
-//     return { url: event.id }
-//   })
+  const eventPaths = events.map((event) => {
+    return { url: event.id }
+  })
 
-//   // Get the paths we want to pre-render based on posts
-//   const paths = eventPaths.map((event) => ({
-//     params: { url: event.url },
-//   }))
+  // Get the paths we want to pre-render based on posts
+  const paths = eventPaths.map((event) => ({
+    params: { url: event.url },
+  }))
 
-//   // We'll pre-render only these paths at build time.
-//   // { fallback: false } means other routes should 404.
-//   return { paths, fallback: false }
-// }
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
 
-// // This also gets called at build time
-// export async function getStaticProps(context) {
+// This also gets called at build time
+export async function getStaticProps(context) {
+  const { locale, locales, defaultLocale, preview = null } = context
+  // params contains the event `url`.
+  // If the route is like /event/1, then params.event is 1
+  // const res = await fetch(`https://.../events/${params.url}`)
+  const eventData = await getAuthorizationToken(context.params.url)
+
+  // Get the navbar and footer from strapi
+  const globalLocale = await getGlobalData(locale)
+  const params = { slug: ["calendar"] }
+  const pageData = await getPageData({ slug: params.slug }, locale, preview)
+
+  // We have the required page data, pass it to the page component
+  const { metadata, localizations, slug } = pageData
+
+  const pageContext = {
+    locale: pageData.locale,
+    locales,
+    defaultLocale,
+    slug,
+    localizations,
+  }
+
+  const event = { url: context.params.url, event: eventData }
+
+  // Pass post data to the page via props
+  return {
+    props: {
+      event,
+      global: globalLocale,
+      metadata,
+      pageContext: {
+        ...pageContext,
+      },
+    },
+    revalidate: 10,
+  }
+}
+
+// export async function getServerSideProps(context) {
 //   const { locale, locales, defaultLocale, preview = null } = context
 //   // params contains the event `url`.
 //   // If the route is like /event/1, then params.event is 1
@@ -110,43 +149,5 @@ function Eventpage({ global, event, pageContext, metadata }) {
 //     },
 //   }
 // }
-
-export async function getServerSideProps(context) {
-  const { locale, locales, defaultLocale, preview = null } = context
-  // params contains the event `url`.
-  // If the route is like /event/1, then params.event is 1
-  // const res = await fetch(`https://.../events/${params.url}`)
-  const eventData = await getAuthorizationToken(context.params.url)
-
-  // Get the navbar and footer from strapi
-  const globalLocale = await getGlobalData(locale)
-  const params = { slug: ["calendar"] }
-  const pageData = await getPageData({ slug: params.slug }, locale, preview)
-
-  // We have the required page data, pass it to the page component
-  const { metadata, localizations, slug } = pageData
-
-  const pageContext = {
-    locale: pageData.locale,
-    locales,
-    defaultLocale,
-    slug,
-    localizations,
-  }
-
-  const event = { url: context.params.url, event: eventData }
-
-  // Pass post data to the page via props
-  return {
-    props: {
-      event,
-      global: globalLocale,
-      metadata,
-      pageContext: {
-        ...pageContext,
-      },
-    },
-  }
-}
 
 export default Eventpage
