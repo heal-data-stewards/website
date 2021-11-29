66ae0c32599d8664bb640388a4a3b9dba0358fd9
Sergio Rocha <rochascript@gmail.com>
2021-11-29 10:36:15 -0600

    Ran lint fix and added a link to each glossary text

--- components/elements/glossary/item.js
+++ components/elements/glossary/item.js
@@ -6,46 +6,46 @@ export default function GlossaryItem({ classes, word }) {
   console.log(word)
   return (
     <>
-    <div className={"anchor"} id={word.anchor}/>
-    <div className={classes.root + " mt-10"} >
-      <h2
-        className="text-magenta capitalize pb-2"
-        style={{ fontWeight: "600", fontSize: "1.3rem" }}
-      >
-        {word.term_name}
-        {/* <a href={}>{word.term_name}</a> */}
-      </h2>
-      <Divider />
-      <div className="pt-4 glossary-markdown">
-        <Markdown>{word.term_body}</Markdown>
+      <div className={"anchor"} id={word.anchor} />
+      <div className={classes.root + " mt-10"}>
+        <h2
+          className="text-magenta capitalize pb-2"
+          style={{ fontWeight: "600", fontSize: "1.3rem" }}
+        >
+          <a href={`/glossary#${word.anchor}`}>{word.term_name}</a>
+        </h2>
+        <Divider />
+        <div className="pt-4 glossary-markdown">
+          <Markdown>{word.term_body}</Markdown>
+        </div>
+        {word.gtd_sources.length !== 0 &&
+          (word.gtd_sources.length > 1 ? (
+            <div>
+              <br /> <b>Sources: </b>
+              <span>
+                {word.gtd_sources.map((source, i) => {
+                  return (
+                    <div key={source.title + i}>
+                      <a href={source.url} style={{ color: "#0044B3" }}>
+                        {source.title}
+                      </a>
+                      {word.gtd_sources.length - 1 !== i ? " & " : ""}
+                    </div>
+                  )
+                })}
+              </span>
+            </div>
+          ) : (
+            <div>
+              <br /> <b>Source: </b>
+              <span>
+                <a href={word.gtd_sources[0].url} style={{ color: "#0044B3" }}>
+                  {word.gtd_sources[0].title}
+                </a>
+              </span>
+            </div>
+          ))}
       </div>
-      {word.gtd_sources.length !== 0 &&
-        (word.gtd_sources.length > 1 ? (
-          <div>
-            <br /> <b>Sources: </b>
-            <span>
-              {word.gtd_sources.map((source, i) => {
-                return (
-                  <div key={source.title + i}>
-                    <a href={source.url} style={{ color: "#0044B3" }}>
-                      {source.title}
-                    </a>
-                    {word.gtd_sources.length - 1 !== i ? " & " : ""}
-                  </div>
-                )
-              })}
-            </span>
-          </div>
-        ) : (
-          <div>
-            <br /> <b>Source: </b>
-            <span>
-              <a href={word.gtd_sources[0].url} style={{ color: "#0044B3" }}>
-                {word.gtd_sources[0].title}
-              </a>
-            </span>
-          </div>
-        ))}
-    </div></>
+    </>
   )
 }
