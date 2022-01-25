45ad225298ac8cf6b8a28278c2d7de3e65f6fc27
Sergio Rocha <rochascript@gmail.com>
2022-01-25 11:07:36 -0600

    emails open in a new window

--- components/elements/navbar.js
+++ components/elements/navbar.js
@@ -1,27 +1,102 @@
-import React, { useState, useEffect } from "react"
-import PropTypes from "prop-types"
-import Link from "next/link"
-import { useRouter } from "next/router"
-import Image from "next/image"
-import { mediaPropTypes, linkPropTypes, buttonLinkPropTypes } from "utils/types"
-import { MdMenu } from "react-icons/md"
-import NextImage from "./image"
-import CustomLink from "./custom-link"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import Drawer from "@material-ui/core/Drawer"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemText from "@material-ui/core/ListItemText"
-import Divider from "@material-ui/core/Divider"
-import { signIn, signOut, useSession, getSession } from "next-auth/client"
-import { Btn2 } from "../elements/button"
-import AccountMenu from "./profile"
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import {
+  mediaPropTypes,
+  linkPropTypes,
+  buttonLinkPropTypes,
+} from "utils/types";
+import { MdMenu } from "react-icons/md";
+import NextImage from "./image";
+import CustomLink from "./custom-link";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { signIn, signOut, useSession, getSession } from "next-auth/client";
+import { Btn2 } from "../elements/button";
+import AccountMenu from "./profile";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { styled, alpha } from '@mui/material/styles';
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    // anchorOrigin={{
+    //   vertical: 'bottom',
+    //   horizontal: 'right',
+    // }}
+    // transformOrigin={{
+    //   vertical: 'top',
+    //   horizontal: 'right',
+    // }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 0,
+    // marginTop: "-9px",
+    padding: '10px 8px',
+    minWidth: 180,
+    background: "#532565",
+    color:
+      "#fff",
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      // padding: '0 0 5px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        // fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:hover': {
+        color: "#e5e0e7",
+      },
+    },
+  },
+}));
+
+const MenuPopupState = (data) => {
+  return (
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <button>
+            <div
+              style={{ fontSize: "18px", fontWeight: "bold" }}
+              className="hover:text-magenta text-purple px-2 py-1"
+              {...bindTrigger(popupState)}
+            >
+              ABOUT
+            </div>
+          </button>
+          <StyledMenu {...bindMenu(popupState)} >
+            <MenuItem onClick={popupState.close}>
+              <Link href="/[[...slug]]" as={"/collective"} >
+                <a style={{ fontSize: "14px", fontWeight: "bold" }} >COLLECTIVE BOARD</a>
+              </Link>
+            </MenuItem>
+          </StyledMenu>
+        </React.Fragment>
+      )}
+    </PopupState>
+  );
+};
 
 const Navbar = ({ navbar, pageContext }) => {
-  const [session, loading] = useSession()
-  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false)
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [session, loading] = useSession();
+  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [navigationItems, setNavigationItems] = useState([
     {
       id: 27,
@@ -35,11 +110,11 @@ const Navbar = ({ navbar, pageContext }) => {
       newTab: false,
       text: "CALENDAR",
     },
-  ])
+  ]);
 
   useEffect(() => {
     if (session || loggedIn) {
-      setNavigationItems(navbar.links)
+      setNavigationItems(navbar.links);
     } else {
       setNavigationItems([
         {
@@ -54,13 +129,13 @@ const Navbar = ({ navbar, pageContext }) => {
           newTab: false,
           text: "CALENDAR",
         },
-      ])
+      ]);
     }
-  }, [session, navbar.links, loggedIn])
-  const router = useRouter()
+  }, [session, navbar.links, loggedIn]);
+  const router = useRouter();
   const handleLogOut = () => {
-    signOut({ redirect: false })
-  }
+    signOut({ redirect: false });
+  };
 
   return (
     <div>
@@ -97,6 +172,9 @@ const Navbar = ({ navbar, pageContext }) => {
                   </CustomLink>
                 </li>
               ))}
+              <li>
+                <MenuPopupState />
+              </li>
             </ul>
           </div>
           <div className="flex">
@@ -187,8 +265,8 @@ const Navbar = ({ navbar, pageContext }) => {
         </Drawer>
       )}
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   navbar: PropTypes.shape({
@@ -200,6 +278,6 @@ Navbar.propTypes = {
     button: buttonLinkPropTypes,
   }),
   initialLocale: PropTypes.string,
-}
+};
 
-export default Navbar
+export default Navbar;
