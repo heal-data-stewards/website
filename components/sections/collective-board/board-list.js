47519d24fc8c296b07d58e1c5b47a237968a34a1
Sergio Rocha <rochascript@gmail.com>
2022-01-27 17:03:44 -0600

    Created basic layout for the public members list

--- components/sections/collective-board/board-list.js
+++ components/sections/collective-board/board-list.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react"
 import List from "@mui/material/List"
-import WorkingGroupListItem from "./list-item"
+import WorkingGroupListItem from "../table/list-item"
 import { getAllUsers } from "utils/api"
 import Button from "@mui/material/Button"
+import PublicWorkingGroupListItem from "./list-item"
 
-export default function WorkingGroupTable({ data }) {
+export default function Boardlist({ data }) {
   const [users, setUsers] = useState([])
   // Call the strapi API to GET all users
   useEffect(() => {
     getAllUsers().then(function (result) {
       const users = result.filter(
-        (user) => user.workgroup === data.title.workinggroup
+        (user) => user.workgroup === "COLLECTIVE_BOARD"
       )
       setUsers(users)
     })
-  }, [data.title.workinggroup])
+  }, [])
 
   const mailTo = users
     .map((user) => {
@@ -29,16 +30,11 @@ export default function WorkingGroupTable({ data }) {
           Co-PIs
         </h1>
       </section> */}
-      <section>
-        <h1 className="text-2xl text-purple p-2" style={{ fontWeight: "600" }}>
-          Members
-        </h1>
-      </section>
       <nav aria-label="working group list">
         <List className="flex flex-wrap">
           {users.map((member, index) => {
             return (
-              <WorkingGroupListItem
+              <PublicWorkingGroupListItem
                 key={member.firstname + index}
                 name={member.firstname + " " + member.lastname}
                 organization={member.organization}
@@ -49,28 +45,6 @@ export default function WorkingGroupTable({ data }) {
           })}
         </List>
       </nav>
-      <section className="mb-8 mt-8">
-        <p className="pr-3 pb-3">
-          To copy a list of email addresses for all collaboration group members,
-          right click the &apos;EMAILS&apos; button and select &apos;Copy Email
-          Address&apos; from the menu.
-        </p>
-        <Button variant="contained" href={`mailto:${mailTo}`}>
-          Emails
-        </Button>
-      </section>
-      <section
-        style={{
-          margin: "0 0 25px 0",
-        }}
-      >
-        <h1
-          className="text-3xl text-purple p-3"
-          style={{ background: "#e5e0e7", fontWeight: "600" }}
-        >
-          Resources
-        </h1>
-      </section>
     </div>
   )
 }
