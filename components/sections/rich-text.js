83332cff9c362058f922619981f7de7fe8400f18
Sergio Rocha <rochascript@gmail.com>
2022-04-11 15:01:32 -0500

    Made rich text links open in new window

--- components/sections/rich-text.js
+++ components/sections/rich-text.js
@@ -4,7 +4,7 @@ import Markdown from "react-markdown"
 const RichText = ({ data }) => {
   return (
     <div className="prose-lg container pb-12 event-html text-gray-dark text-xl">
-      <Markdown>{data.content}</Markdown>
+      <Markdown linkTarget="_blank">{data.content}</Markdown>
     </div>
   )
 }
