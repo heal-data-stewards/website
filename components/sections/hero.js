99a0c7c33ad7865179316739d4467efc4a8db4df
Sergio Rocha <rochascript@gmail.com>
2022-04-29 16:34:20 -0500

    tooltips now populate based on glossary definitions available

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -2,8 +2,10 @@ import Markdown from "react-markdown"
 import { getButtonAppearance } from "utils/button"
 import ButtonLink from "../elements/button-link"
 import NextImage from "../elements/image"
+import React from 'react'
 
 const Hero = ({ data }) => {
+
   return (
     <main className="container flex flex-col md:flex-row items-center justify-between py-12">
       {/* Left column for content */}
