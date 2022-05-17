b1a61130690f0e7728614532253d5d6bbcf74f48
Sergio Rocha <rochascript@gmail.com>
2022-05-17 17:19:08 -0500

    HEAL Data Repository Selection Guide page is up

--- components/sections/table/general-table.js
+++ components/sections/table/general-table.js
@@ -1,39 +1,54 @@
-import React from "react";
-import { DataGrid, GridToolbar } from "@material-ui/data-grid";
+import React from "react"
+import { DataGrid, GridToolbar } from "@material-ui/data-grid"
 import Markdown from "react-markdown"
 
 const columns = [
   { field: "id", hide: true, headerName: "ID", width: 10 },
-  { field: "Repository", headerName: "Repository", width: 150, renderCell: ({ row }) => (<Markdown linkTarget="_blank">{row.Repository}</Markdown>) },
+  {
+    field: "Repository",
+    headerName: "Repository",
+    width: 150,
+    // eslint-disable-next-line react/display-name
+    renderCell: ({ row }) => (
+      <Markdown linkTarget="_blank">{row.Repository}</Markdown>
+    ),
+  },
   { field: "Repository Type", headerName: "Repository Type", width: 200 },
   { field: "Organism", headerName: "Organism", width: 150 },
   { field: "IC/Program", headerName: "IC/Program", width: 150 },
-  { field: "Overview", headerName: "Overview", width: 150, renderCell: ({ row }) => (<Markdown linkTarget="_blank">{row.Overview}</Markdown>) },
-];
+  {
+    field: "Overview",
+    headerName: "Overview",
+    width: 150,
+    // eslint-disable-next-line react/display-name
+    renderCell: ({ row }) => (
+      <Markdown linkTarget="_blank">{row.Overview}</Markdown>
+    ),
+  },
+]
 
 function createData(id, data) {
-  let row = {...data};
+  let row = { ...data }
 
   for (const property in row) {
-      let index = Number(property) + 1;
-      let newKey = columns[index].field;
-      row[newKey] = row[property];
-      delete row[property]
+    let index = Number(property) + 1
+    let newKey = columns[index].field
+    row[newKey] = row[property]
+    delete row[property]
   }
   row["id"] = id
-  
-  return row;
+
+  return row
 }
 
 export default function GeneralDataTable(data) {
-
   let test = data.data.row.map((row, i) => {
     let bucket = row.columns.map((column, i) => {
-      return column.column_data;
-    });
+      return column.column_data
+    })
 
-    return createData(i, bucket);
-  });
+    return createData(i, bucket)
+  })
   return (
     <div style={{ height: 600, width: "100%" }} className={"container mb-8"}>
       <DataGrid
@@ -46,5 +61,5 @@ export default function GeneralDataTable(data) {
         }}
       />
     </div>
-  );
+  )
 }
