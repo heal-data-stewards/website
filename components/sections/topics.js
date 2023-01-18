dd32a2f121919ec02bc9bf689f82c7623fb000b8
Sergio Rocha <rochascript@gmail.com>
2023-01-18 10:49:26 -0600

    Added a search block to the search page

--- components/sections/topics.js
+++ components/sections/topics.js
@@ -1,10 +1,23 @@
 import React from "react"
 import Markdown from "react-markdown"
+import { Divider } from "@material-ui/core"
 
 export default function Topics({ data }) {
   // add error handling
   return (
     <>
+      <div
+        className="bg-gray-100 pt-12 pb-12"
+        style={{ background: "#c0b3c569" }}
+      >
+        <div className="container">
+          <h4 className="text-2xl text-magenta font-bold mb-4">
+            Search Results
+          </h4>
+          <Divider style={{ background: "black" }} />
+          <p className="mt-4">0 results found</p>
+        </div>
+      </div>
       <main className="container pb-12 ">
         <h2 className="text-4xl text-purple font-bold mb-4 mt-14">
           {data.title}
