54e9caffa400ba83c42ea1db33bd620ef40a545f
suejinkim20 <kim.suejin.20@gmail.com>
2023-07-11 16:19:59 -0400

    run lint:fix

--- components/elements/markdown.js
+++ components/elements/markdown.js
@@ -1,5 +1,5 @@
-import ReactMarkdown from 'react-markdown'
-import React, { useMemo } from 'react'
+import ReactMarkdown from "react-markdown"
+import React, { useMemo } from "react"
 
 const Markdown = ({ src }) => {
   const componentMap = useMemo(() => ({
