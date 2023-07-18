cbdbadbdf7d488853905d7678b9799e6dbdf4670
suejinkim20 <kim.suejin.20@gmail.com>
2023-07-18 09:11:54 -0400

    update spacing on markdown component map

--- components/elements/markdown.js
+++ components/elements/markdown.js
@@ -2,12 +2,25 @@ import ReactMarkdown from "react-markdown"
 import React, { useMemo } from "react"
 
 const Markdown = ({ children }) => {
-  const componentMap = useMemo(() => ({
-    p: function Anchor({ node, href, children, ...props }) { return <p style={{
-      paddingBottom: '1rem',
-    }}>{children}</p> },
-    li: function Anchor({ node, href, children, ...props }) { return <li style={{lineHeight: '1.5', letterSpacing: '0.2px' }}>{children}</li> }
-  }), [])
+  const componentMap = useMemo(
+    () => ({
+      p: function Anchor({ node, href, children, ...props }) {
+        return (
+          <p style={{ paddingBottom: "1rem" }}>
+            {children}
+          </p>
+        )
+      },
+      li: function Anchor({ node, href, children, ...props }) {
+        return (
+          <li style={{ lineHeight: "1.5", letterSpacing: "0.2px" }}>
+            {children}
+          </li>
+        )
+      },
+    }),
+    []
+  )
 
   return (
     <ReactMarkdown components={ componentMap }>
