3ae095a54496b08d17eca71af87abc64d201ecf6
Sergio Rocha <rochascript@gmail.com>
2023-08-01 09:06:40 -0500

    linted code, sensitive data page completed

--- components/sections/sensitive-data.js
+++ components/sections/sensitive-data.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 
-const Block = ({ title }) => {
+const Block = ({ title, onMouseEnter }) => {
   return (
     <div
       style={{
@@ -9,7 +9,12 @@ const Block = ({ title }) => {
         padding: "15px",
         height: "75px",
         background: "#e5e0e7",
+        cursor: "pointer",
+        color: "rgba(83, 37, 101, 1)",
+        maxWidth: "300px",
       }}
+      className={"sensitive-data-blocks"}
+      onMouseEnter={onMouseEnter}
     >
       {title}
     </div>
@@ -17,16 +22,117 @@ const Block = ({ title }) => {
 }
 
 const SensitiveData = ({ data }) => {
-  console.log(data)
+  const [shownContent, setShownContent] = useState(data.sensitiveDataItem[0])
+
+  function onHover(item) {
+    setShownContent(item)
+  }
+
   return (
     <div className="prose-lg container pb-12 event-html text-gray-dark text-xl">
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ width: "40%" }}>
+        <div style={{ width: "50%", maxWidth: "350px", marginRight: "20px" }}>
           {data.sensitiveDataItem.map((item, i) => {
-            return <Block key={i + item.title} title={item.title} />
+            return (
+              <Block
+                onMouseEnter={(e) => onHover(item)}
+                key={i + item.title}
+                title={item.title}
+              />
+            )
           })}
         </div>
-        <div style={{ width: "55%" }}>body goes here</div>
+        <div style={{ width: "-webkit-fill-available" }}>
+          <h1
+            style={{
+              marginBottom: "5px",
+              fontSize: "21px",
+              fontWeight: "bold",
+            }}
+          >
+            {shownContent.title}
+          </h1>
+          <div style={{ marginBottom: "30px" }}>{shownContent.body}</div>
+          <div>
+            <h2
+              style={{
+                margin: "0 0 3px 0",
+                fontSize: "19px",
+                fontWeight: "bold",
+              }}
+            >
+              Resources
+            </h2>
+            <hr style={{ marginBottom: 0 }} />
+            {shownContent.sensitiveDataResources.length > 0 && (
+              <ul>
+                {shownContent.sensitiveDataResources.map((resource, i) => {
+                  return (
+                    <li
+                      key={resource.link + i}
+                      style={{
+                        padding: 0,
+                        color: "#9a256b",
+                        margin: "0 0 0 16px",
+                      }}
+                    >
+                      <a
+                        style={{
+                          color: "#9a256b",
+                        }}
+                        target={"_blank"}
+                        href={resource.link}
+                        rel="noreferrer"
+                      >
+                        {resource.link}
+                      </a>
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
+          <div>
+            <h2
+              style={{
+                margin: "0 0 3px 0",
+                fontSize: "19px",
+                fontWeight: "bold",
+              }}
+            >
+              HEAL-specific Resources
+            </h2>
+            <hr style={{ marginBottom: 0 }} />
+
+            {shownContent.sensitiveDataResources.length > 0 && (
+              <ul>
+                {shownContent.sensitiveDataResources.map((resource, i) => {
+                  return (
+                    <li
+                      key={resource.link + i}
+                      style={{
+                        padding: 0,
+                        color: "#9a256b",
+                        margin: "0 0 0 16px",
+                      }}
+                    >
+                      <a
+                        style={{
+                          color: "#9a256b",
+                        }}
+                        target={"_blank"}
+                        href={resource.link}
+                        rel="noreferrer"
+                      >
+                        {resource.link}
+                      </a>
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   )
