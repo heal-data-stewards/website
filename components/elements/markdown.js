a291d06598fa09e46e1293cefe44e096cb293728
suejinkim20 <kim.suejin.20@gmail.com>
2023-08-03 16:02:22 -0400

    WIP implement link filtering logic for external links

--- components/elements/markdown.js
+++ components/elements/markdown.js
@@ -1,22 +1,78 @@
 import ReactMarkdown from "react-markdown"
-import React, { useMemo } from "react"
+import React, { Fragment } from "react"
+import PropTypes from 'prop-types'
+import NextLink from 'next/link'
+
+const InternalLink = React.forwardRef(function InternalLink({ children, className, ...props }, ref) {
+  return (
+    <NextLink { ...props }>
+      <a style={{color: 'red'}} ref={ ref }>{ children }</a>
+    </NextLink>
+  )
+})
+
+//
+
+const ExternalLink = ({ href, children, ...props }) => {
+  return (
+    <Fragment>
+      <a
+        href={ href }
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{color: 'green'}}
+        { ...props }
+      >
+        { children }
+      </a>
+    </Fragment>
+  )
+}
+
+//
+
+const CustomLink = React.forwardRef(function CustomLink({ to, children, ...props }, ref) {
+  const mailtoPattern = new RegExp(/^mailto:/)
+  const externalUrlPattern = new RegExp(/^https?:\/\//)
+  const externalUrlMatch = externalUrlPattern.exec(to)
+  const mailtoMatch = mailtoPattern.exec(to)
+  const LinkComponent = externalUrlMatch || mailtoMatch ? ExternalLink : InternalLink
+  return (
+    <LinkComponent href={ to } to={ to } ref={ ref } { ...props }>
+      { children }
+    </LinkComponent>
+  )
+})
+
+CustomLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+ExternalLink.propTypes = CustomLink.propTypes
+ExternalLink.defaultProps = CustomLink.defaultProps
+
+
+
+// this object defines a map:
+//    DOM elements --> React components.
+// this allows us to streamline styles for content coming
+// from Strapi with that of content built here by Nextjs.
+
+const componentMap = {
+  // for links, we'll use our smart link component.
+  a: ({ node, href, ...props }) => (
+    <CustomLink
+      to={ href }
+      { ...props }
+    />
+  ),
+  p: ({ node, children, ...props })=>(
+    <p style={{ paddingBottom: "1rem" }}>{children}</p>
+  ),
+}
 
 const Markdown = ({ children }) => {
-  const componentMap = useMemo(
-    () => ({
-      p: function Anchor({ node, href, children, ...props }) {
-        return <p style={{ paddingBottom: "1rem" }}>{children}</p>
-      },
-      li: function Anchor({ node, href, children, ...props }) {
-        return (
-          <li style={{ lineHeight: "1.5", letterSpacing: "0.2px" }}>
-            {children}
-          </li>
-        )
-      },
-    }),
-    []
-  )
 
   return <ReactMarkdown components={componentMap}>{children}</ReactMarkdown>
 }
