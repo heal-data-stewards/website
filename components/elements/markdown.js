dbf34546142c70e310e856db570bfe53ad7f4d26
suejinkim20 <kim.suejin.20@gmail.com>
2023-08-07 14:20:58 -0400

    abstract out link logic from markdown component

--- components/elements/markdown.js
+++ components/elements/markdown.js
@@ -1,57 +1,8 @@
 import ReactMarkdown from "react-markdown"
 import React, { Fragment } from "react"
-import PropTypes from "prop-types"
-import NextLink from "next/link"
+import Typography from "@material-ui/core/Typography"
+import Link from "../elements/link"
 
-const InternalLink = React.forwardRef(function InternalLink(
-  { children, className, ...props },
-  ref
-) {
-  return (
-    <NextLink {...props}>
-      <a ref={ref}>{children}</a>
-    </NextLink>
-  )
-})
-
-//
-
-const ExternalLink = ({ href, children, ...props }) => {
-  return (
-    <Fragment>
-      <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
-        {children}
-      </a>
-    </Fragment>
-  )
-}
-
-//
-
-const CustomLink = React.forwardRef(function CustomLink(
-  { to, children, ...props },
-  ref
-) {
-  const mailtoPattern = new RegExp(/^mailto:/)
-  const externalUrlPattern = new RegExp(/^https?:\/\//)
-  const externalUrlMatch = externalUrlPattern.exec(to)
-  const mailtoMatch = mailtoPattern.exec(to)
-  const LinkComponent =
-    externalUrlMatch || mailtoMatch ? ExternalLink : InternalLink
-  return (
-    <LinkComponent href={to} to={to} ref={ref} {...props}>
-      {children}
-    </LinkComponent>
-  )
-})
-
-CustomLink.propTypes = {
-  to: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-}
-
-ExternalLink.propTypes = CustomLink.propTypes
-ExternalLink.defaultProps = CustomLink.defaultProps
 
 // this object defines a map:
 //    DOM elements --> React components.
@@ -60,9 +11,9 @@ ExternalLink.defaultProps = CustomLink.defaultProps
 
 const componentMap = {
   // for links, we'll use our smart link component.
-  a: ({ node, href, ...props }) => <CustomLink to={href} {...props} />,
+  a: ({ node, href, ...props }) => <Link to={href} {...props} />,
   p: ({ node, children, ...props }) => (
-    <p style={{ paddingBottom: "1rem" }}>{children}</p>
+    <Typography style={{ paddingBottom: "1rem" }}>{children}</Typography>
   ),
 }
 
