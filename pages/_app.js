14f1ff3e6a7fd99b4e19cf71ec38a3179a59e22d
suejinkim20 <kim.suejin.20@gmail.com>
2023-08-07 15:45:59 -0400

    import google font a different way, using link

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,8 @@ import { getStrapiMedia } from "utils/media"
 import { getGlobalData } from "utils/api"
 import { Provider } from "next-auth/client"
 // import { useEffect } from "react"
-import "@/styles/index.css"
 import { RouteGuard } from "@/components/route-guard"
+import "@/styles/index.css"
 
 const MyApp = ({ Component, pageProps }) => {
   // const router = useRouter()
@@ -36,6 +36,9 @@ const MyApp = ({ Component, pageProps }) => {
       {/* Favicon */}
       <Head>
         <link rel="shortcut icon" href={getStrapiMedia(global.favicon.url)} />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>  
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       </Head>
       {/* Global site metadata */}
       <DefaultSeo
