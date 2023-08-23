e848e42cb0c7c384134f81c8f705edd44c62e400
Sergio Rocha <rochascript@gmail.com>
2023-08-23 11:05:23 -0500

    created a dug search component and wired it up to strapi

---
+++ components/sections/dug-search.js
@@ -0,0 +1,11 @@
+const DugSearch = ({ data }) => {
+  return (
+    <div className="prose-lg container pb-12 event-html text-gray-dark text-xl">
+      <form>
+        <input type="text" id="dug-search" placeholder={data.placeholder} />
+      </form>
+    </div>
+  );
+};
+
+export default DugSearch;
