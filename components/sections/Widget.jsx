64b9d775b5722c4e1d5ae07e574e0f1d93bee6f4
Sergio Rocha <rochascript@gmail.com>
2023-09-05 12:39:27 -0500

    changed the color of the widget

--- components/sections/Widget.jsx
+++ components/sections/Widget.jsx
@@ -13,7 +13,7 @@ export function Widget({ data }) {
         <WidgetForm data={data} />
       </Popover.Panel>
 
-      <Popover.Button className="bg-[#532565] rounded-full px-3 h-12 text-white flex items-center group">
+      <Popover.Button className="bg-[#992568] rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
