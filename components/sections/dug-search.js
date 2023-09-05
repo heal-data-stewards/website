859188ae56a050e2c281b68c43296689a78f8b8e
suejinkim20 <kim.suejin.20@gmail.com>
2023-09-05 12:57:42 -0400

    add feedback message line

--- components/sections/dug-search.js
+++ components/sections/dug-search.js
@@ -1,23 +1,44 @@
-import React, { Fragment, useEffect, useRef } from "react"
+import React, { useRef, useState } from "react"
 import Paper from "@mui/material/Paper"
 import InputBase from "@mui/material/InputBase"
 import ButtonBase from "@mui/material/ButtonBase"
+import Typography from "@mui/material/Typography"
+
+const FeedbackLine = ({ message }) => {
+  return (
+    <Typography variant="caption" sx={{ color: "#982568" }}>
+      {message}
+    </Typography>
+  )
+}
 
 const DugSearch = ({ data }) => {
   const inputField = useRef()
+  const [errorMessage, setErrorMessage] = useState("")
 
   const doSearch = (event) => {
     event.preventDefault()
+    //checks if the inputField has rendered
     if (!inputField.current) {
       return
     }
+
+    //checks if the user has entered any value here (it can be a bunch of spaces and  still be considered a value)
+    //this only returns if the user hasnt entered any characters at all
     if (!inputField.current.value) {
+      setErrorMessage("No value indicated. Please enter search term.")
       return
     }
+
+    //this trims any spaces off the front or back of the words
+    //if you enter a bunch of spaces, you wont get sent to the heal dug page
     const trimmedString = inputField.current.value.trim()
     if (!trimmedString) {
+      setErrorMessage("No value indicated. Please enter search term.")
       return
     }
+
+    //this only sends you to the heal-dug page with your trimmed version of the search term
     window.location = `https://heal.renci.org/?q=${trimmedString}`
   }
 
@@ -64,6 +85,7 @@ const DugSearch = ({ data }) => {
           Search
         </ButtonBase>
       </Paper>
+      <FeedbackLine message={errorMessage} />
     </div>
   )
 }
