f414962a77df5a55aa15003e52c017ed55c2713d
Sergio Rocha <rochascript@gmail.com>
2023-09-12 02:16:51 -0500

    made the feedback widget look more like a feedback widget instead of chat widget

--- components/sections/Widget.jsx
+++ components/sections/Widget.jsx
@@ -1,26 +1,29 @@
-import { ChatTeardropDots } from "phosphor-react"
+import { ChatTeardropDots } from "phosphor-react";
 
-import { Popover } from "@headlessui/react"
-import { WidgetForm } from "./WidgetForm"
+import { Popover } from "@headlessui/react";
+import { WidgetForm } from "./WidgetForm";
 
 export function Widget({ data }) {
   return (
     <Popover
-      style={{ right: "0", marginRight: "20px", bottom: "20px", zIndex: 100 }}
+      style={{ right: "-20px", bottom: "106px", zIndex: 100 }}
       className="fixed flex flex-col items-end"
     >
       <Popover.Panel>
         <WidgetForm data={data} />
       </Popover.Panel>
 
-      <Popover.Button className="bg-[#992568] rounded-full px-3 h-12 text-white flex items-center group">
-        <ChatTeardropDots className="w-6 h-6" />
-
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+      <Popover.Button
+        style={{ transform: "rotate(-90deg)" }}
+        className="bg-[#992568] px-3 h-10 text-white flex items-center group"
+      >
+        {/* <ChatTeardropDots className="w-6 h-6" /> */}
+        {data.buttonText}
+        {/* <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
           {data.buttonText}
-        </span>
+        </span> */}
       </Popover.Button>
     </Popover>
-  )
+  );
 }
