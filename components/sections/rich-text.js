842f40b8d49f260e8d59cbd8444d39a93839a23b
Sergio Rocha <rochascript@gmail.com>
2023-09-20 10:39:48 -0500

    changed text size for rich text components

--- components/sections/rich-text.js
+++ components/sections/rich-text.js
@@ -3,7 +3,7 @@ import Markdown from "../elements/markdown"
 
 const RichText = ({ data }) => {
   return (
-    <div className="prose-lg container pb-12 event-html text-gray-dark text-xl">
+    <div className="rich-text prose-lg container pb-12 event-html text-gray-dark text-xl">
       <Markdown>{data.content}</Markdown>
     </div>
   )
