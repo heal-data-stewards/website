b8d2e003b87d704f6ceabd7ed1fa437bf9f8e0a2
suejinkim20 <kim.suejin.20@gmail.com>
2023-10-17 11:15:40 -0400

    update typography elements

--- components/sections/page-title.js
+++ components/sections/page-title.js
@@ -6,13 +6,15 @@ import Typography from "@mui/material/Typography"
 export default function PageTitle({ data, noPaddingBottom, glossary }) {
   return (
     <section className={`container pt-10 ${!noPaddingBottom && "pb-10"}`}>
-      <Typography variant="h1" className="pb-3">
+      <Typography variant="h1" className="pb-2">
         {data.title}
       </Typography>
       <Divider />
-      <p className="text-xl text-gray-dark pt-4">
-        {addToolTips(data.optionaldescription, glossary)}
-      </p>
+      {data.optionaldescription && (
+        <Typography variant="body1" className="text-xl text-gray-dark pt-4">
+          {addToolTips(data.optionaldescription, glossary)}
+        </Typography>
+      )}
     </section>
   )
 }
