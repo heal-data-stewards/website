2db4922c3dbfaaa942e6a0d6bcf0d85d1dd96aad
Sergio Rocha <rochascript@gmail.com>
2023-10-26 07:56:02 -0600

    Update CloseButton.jsx
    changes the title in the popover button

--- components/sections/CloseButton.jsx
+++ components/sections/CloseButton.jsx
@@ -5,7 +5,7 @@ export function CloseButton() {
   return (
     <Popover.Button
       className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"
-      title="Fechar formulário de feedback"
+      title="Close feedback form"
     >
       <X weight="bold" className="w-4 h-4" />
     </Popover.Button>
