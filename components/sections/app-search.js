9dec00ebd2a9689bd268cc02d031237f15c9dfc0
Sergio Rocha <srocha@renci.org>
2024-01-24 08:39:39 -0600

    API is now being called to populate the table

--- components/sections/app-search.js
+++ components/sections/app-search.js
@@ -73,47 +73,57 @@ export default function AppSearch({ data }) {
   const [tableData, setData] = React.useState(false)
 
   function createData(res) {
-    let bucket = [];
+    let bucket = []
 
     for (const [key, keyValue] of Object.entries(res[0])) {
       switch (key) {
         case "clinical_trials_study_ID":
-          let status = keyValue ? "green" : "yellow";
-          let notes = status == "green" ? "Thank you for registering your study on ClinicalTrials.gov!" : "We do not have a record of an NCT ID for your study. If your study is not a clinical trial, please skip this step!  If your study IS a clinical trial, please register your study on ClinicalTrials.gov as soon as possible, and send the NCT ID to heal-support@datacommons.io so we can update your study on the platform.";
-          let step = "Register Your Study on ClinicalTrials.gov (if appropriate)";
+          let status = keyValue ? "green" : "yellow"
+          let notes =
+            status == "green"
+              ? "Thank you for registering your study on ClinicalTrials.gov!"
+              : "We do not have a record of an NCT ID for your study. If your study is not a clinical trial, please skip this step!  If your study IS a clinical trial, please register your study on ClinicalTrials.gov as soon as possible, and send the NCT ID to heal-support@datacommons.io so we can update your study on the platform."
+          let step =
+            "Register Your Study on ClinicalTrials.gov (if appropriate)"
           bucket.push({ status, step, notes })
-          break;
+          break
         case "hdp_id":
-          let status2 = keyValue ? "green" : "yellow";
-          let notes2 = status == "green" ? "Thank you for registering your study on the HEAL Platform!" : "Please register your study on the HEAL Platform as soon as possible. For registration instructions, click here. If you cannot find your study on the platform, please reach out to heat-support@datacommons.io";
-          let step2 = "Register Your Study With the HEAL Data Platform";
+          let status2 = keyValue ? "green" : "yellow"
+          let notes2 =
+            status == "green"
+              ? "Thank you for registering your study on the HEAL Platform!"
+              : "Please register your study on the HEAL Platform as soon as possible. For registration instructions, click here. If you cannot find your study on the platform, please reach out to heat-support@datacommons.io"
+          let step2 = "Register Your Study With the HEAL Data Platform"
           bucket.push({ status: status2, step: step2, notes: notes2 })
-          break;
+          break
         default:
           break
       }
     }
-    setData(bucket);
+    setData(bucket)
   }
 
   const getAppId = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    let regExp = /[a-zA-Z]/g;
-    let param;
+    let regExp = /[a-zA-Z]/g
+    let param
 
     if (regExp.test(value)) {
       param = "proj_num="
     } else {
-      param = "appl_id=";
+      param = "appl_id="
     }
 
     axios
-      .get(`https://9trlpa4nv4.execute-api.us-east-1.amazonaws.com/dev/checklistv3?${param}${value}`)
+      .get(
+        `https://9trlpa4nv4.execute-api.us-east-1.amazonaws.com/dev/checklistv3?${param}${value}`
+      )
       .then((response) => {
-        setValue(response.data);
-        createData(response.data);
-      }).catch((err) => console.error(err));
+        setValue(response.data)
+        createData(response.data)
+      })
+      .catch((err) => console.error(err))
   }
 
   let handleTextFieldChange = (e) => {
@@ -123,7 +133,12 @@ export default function AppSearch({ data }) {
   return (
     <div className={"container mb-16"}>
       {/* <p style={{marginBottom: "20px", fontSize: "16px"}}>Wondering where your study is on the HEAL Compliance Journey? Try our new checklist companion tool. Just type in your study's unique application id or project number and see results.</p> */}
-      <form noValidate autoComplete="off" onSubmit={getAppId} style={{ marginBottom: "10px" }}>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={getAppId}
+        style={{ marginBottom: "10px" }}
+      >
         <TextField
           id="outlined-basic"
           label="App / Proj Number"
@@ -175,10 +190,9 @@ export default function AppSearch({ data }) {
             //   enableRowSelection //enable some features
             enableColumnOrdering
             enableGlobalFilter={false} //turn off a feature
-          /></>
+          />
+        </>
       )}
-
-
     </div>
   )
 }
