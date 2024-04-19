922d4a8d806d8f87aef3e4e34d932618882a8556
Matt Watson <mwatson@renci.org>
2024-04-19 12:43:22 -0400

    improve feedback button style/behavior

--- components/sections/Widget.jsx
+++ components/sections/Widget.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material"
 import { ChatTeardropDots } from "phosphor-react"
 
 import { Popover } from "@headlessui/react"
@@ -6,16 +7,27 @@ import { WidgetForm } from "./WidgetForm"
 export function Widget({ data }) {
   return (
     <Popover
-      style={{ right: "-20px", bottom: "106px", zIndex: 100 }}
+      style={{ right: "-1rem", bottom: "106px", zIndex: 999 }}
       className="fixed flex flex-col items-end"
     >
       <Popover.Panel>
         <WidgetForm data={data} />
       </Popover.Panel>
 
-      <Popover.Button
-        style={{ transform: "rotate(-90deg)" }}
-        className="bg-[#992568] px-3 h-10 text-white flex items-center group"
+      <Button
+        component={Popover.Button}
+        variant="contained"
+        sx={{
+          transform: "rotate(-90deg) translateY(50px)",
+          transition: "transform 250ms, background-color 250ms",
+          height: "100px",
+          display: "flex",
+          alignItems: "flex-start",
+          "&:hover": {
+            transform: "rotate(-90deg) translateY(40px)",
+          },
+        }}
+        className="bg-[#992568] p-3 h-10 text-white"
       >
         {/* <ChatTeardropDots className="w-6 h-6" /> */}
         {data.buttonText}
@@ -23,7 +35,7 @@ export function Widget({ data }) {
           <span className="pl-2"></span>
           {data.buttonText}
         </span> */}
-      </Popover.Button>
+      </Button>
     </Popover>
   )
 }
