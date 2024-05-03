be4fe20765e9efae42ba346443df2b5b105d8a0a
Matt Watson <mwatson@renci.org>
2024-05-03 09:11:56 -0400

    add back and start over buttons

--- components/sections/repo-questions.js
+++ components/sections/repo-questions.js
@@ -1,5 +1,6 @@
 import Markdown from "../elements/markdown"
 import * as React from "react"
+import { Box } from "@mui/material"
 import Radio from "@mui/material/Radio"
 import RadioGroup from "@mui/material/RadioGroup"
 import FormControlLabel from "@mui/material/FormControlLabel"
@@ -7,12 +8,24 @@ import FormControl from "@mui/material/FormControl"
 import { Button } from "@mui/material"
 import FileDownloadIcon from "@mui/icons-material/FileDownload"
 import { CSVLink, CSVDownload } from "react-csv"
-
+import {
+  KeyboardArrowLeft as BackIcon,
+  RestartAlt as StartOverIcon,
+} from "@mui/icons-material"
 const RepoQuestions = ({ data }) => {
   const [value, setValue] = React.useState("")
   const [showOptions, setShowOptions] = React.useState(true)
   const [optionalInformation, setOptionalInformation] = React.useState(false)
   const [questionToShow, setQuestionToShow] = React.useState(1)
+
+  const handleClickStartOver = () => {
+    setQuestionToShow(1)
+    setOptionalInformation(false)
+    setShowOptions(true)
+  }
+  const handleClickBack = () => {
+    setQuestionToShow(Math.max(questionToShow - 1, 1))
+  }
 
   const handleChange = (event) => {
     setValue(event.target.value)
@@ -27,6 +40,33 @@ const RepoQuestions = ({ data }) => {
       }
     }
   }
+
+  const BackButton = React.useCallback(
+    () => (
+      <Button
+        onClick={handleClickBack}
+        disabled={questionToShow === 1}
+        variant="outlined"
+        startIcon={<BackIcon />}
+      >
+        Back
+      </Button>
+    ),
+    [handleClickBack, questionToShow]
+  )
+
+  const StartOverButton = React.useCallback(
+    () => (
+      <Button
+        onClick={handleClickStartOver}
+        variant="outlined"
+        startIcon={<StartOverIcon />}
+      >
+        Start Over
+      </Button>
+    ),
+    []
+  )
 
   return (
     <div
@@ -71,9 +111,7 @@ const RepoQuestions = ({ data }) => {
                   </FormControl>
                 </div>
               )}
-              {/* {nextButton && (
-                                <button>next button coming soon</button>
-                            )} */}
+
               {optionalInformation && (
                 <div>
                   <Markdown>{optionalInformation}</Markdown>
@@ -85,6 +123,18 @@ const RepoQuestions = ({ data }) => {
                   </Button>
                 </div>
               )}
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 1,
+                  mt: 3,
+                }}
+              >
+                <BackButton />
+                <StartOverButton />
+              </Box>
             </div>
           )
         }
