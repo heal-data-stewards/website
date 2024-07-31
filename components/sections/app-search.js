aa132659b7404c02ca6027d259d34ebe8120cee7
Matt Watson <mwatson@renci.org>
2024-07-31 12:56:56 -0400

    update negative response for clinicaltrials.gov registration step

--- components/sections/app-search.js
+++ components/sections/app-search.js
@@ -184,7 +184,7 @@ export default function AppSearch({ data }) {
         step,
         notes: data.time_of_registration
           ? "Thank you for registering your study on the HEAL Platform!"
-          : "Please register your study on the HEAL Platform as soon as possible. For registration instructions, [click here](https://heal.github.io/platform-documentation/study-registration/). If you cannot find your study on the platform, please reach out to [heal-support@datacommons.io](mailto:heal-support@datacommons.io).",
+          : "Please register your study on the HEAL Platform as soon as possible. There are 3 steps to registration: (1) request access to register the study; (2) create a CEDAR account; and (3) finish registering your study on the platform. For detailed registration instructions, [click here](https://heal.github.io/platform-documentation/study-registration/). If you cannot find your study on the platform, please reach out to [heal-support@datacommons.io](mailto:heal-support@datacommons.io).",
       })
     }
 
