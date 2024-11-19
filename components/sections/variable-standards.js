977b711d0961aae574689f60ceeb5ecd3e1d53a8
David Glymph <Woozl@users.noreply.github.com>
2024-11-19 12:28:51 -0500

    add multi-select

--- components/sections/variable-standards.js
+++ components/sections/variable-standards.js
@@ -7,7 +7,7 @@ const VariableStandards = () => {
 
   return (
     <div className="container pb-4 mt-20">
-      {questions.map((q, i) => (
+      {/* {questions.map((q, i) => (
         <>
           <Question
             number={i + 1}
@@ -18,12 +18,21 @@ const VariableStandards = () => {
           />
           <hr />
         </>
-      ))}
+      ))} */}
+
+      <pre>{JSON.stringify(focusArea, null, 2)}</pre>
+
+      <Question
+        number={1}
+        question={questions[5]}
+        selected={focusArea}
+        setSelected={setFocusArea}
+      />
     </div>
   )
 }
 
-function Question({ number, question, selected, onChange }) {
+function Question({ number, question, selected, setSelected }) {
   return (
     <div className="py-4">
       <p>
@@ -44,14 +53,21 @@ function Question({ number, question, selected, onChange }) {
           answers={question.answers}
           id={question.id}
           selected={selected}
-          onChange={onChange}
+          setSelected={setSelected}
         />
-      ) : null}
+      ) : (
+        <MultipleChoice
+          answers={question.answers}
+          id={question.id}
+          selected={selected}
+          setSelected={setSelected}
+        />
+      )}
     </div>
   )
 }
 
-function SingleChoice({ answers, id, selected, onChange }) {
+function SingleChoice({ answers, id, selected, setSelected }) {
   return (
     <div className="flex flex-col">
       {answers.map((a) => {
@@ -73,7 +89,53 @@ function SingleChoice({ answers, id, selected, onChange }) {
               className="mr-2"
               checked={selected === value.toString()}
               onChange={(e) => {
-                onChange(e.target.value)
+                setSelected(e.target.value)
+              }}
+            />
+            {answer}
+          </label>
+        )
+      })}
+    </div>
+  )
+}
+
+function MultipleChoice({ answers, id, selected, setSelected }) {
+  const toggleChoice = (value) => {
+    if (selected === undefined) {
+      setSelected([value])
+      return
+    }
+
+    if (!selected.includes(value)) {
+      setSelected((prev) => [...prev, value])
+    } else {
+      setSelected((prev) => prev.filter((item) => item !== value))
+    }
+  }
+
+  return (
+    <div className="flex flex-col">
+      {answers.map((a) => {
+        let answer, value
+        if (typeof a === "string") {
+          answer = a
+          value = a
+        } else {
+          answer = a.answer
+          value = a.value
+        }
+
+        return (
+          <label key={value}>
+            <input
+              type="checkbox"
+              name={id}
+              value={value}
+              className="mr-2"
+              checked={selected !== undefined && selected.includes(value)}
+              onChange={(e) => {
+                toggleChoice(e.target.value)
               }}
             />
             {answer}
