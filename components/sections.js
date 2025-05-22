14fecc555cfb36a10d0e2a4c6a28c99aa7ce205a
suejinkim20 <kim.suejin.20@gmail.com>
2025-05-22 16:00:05 -0400

    separate status tracker tool from roadmap component

--- components/sections.js
+++ components/sections.js
@@ -44,6 +44,7 @@ import RepoQuestions from "./sections/repo-questions"
 import FloatingInfoBox from "./sections/floating-info-box"
 import VerticalTabs from "./sections/vertical-tabs"
 import Footnotes from "./sections/footnotes"
+import DataSharingStatus from "./sections/data-sharing-status"
 
 // Map Strapi sections to section components
 const sectionComponents = {
@@ -92,6 +93,7 @@ const sectionComponents = {
   "sections.floating-info-box": FloatingInfoBox,
   "sections.vertical-tabs": VerticalTabs,
   "sections.footnotes": Footnotes,
+  "elements.data-sharing-status": DataSharingStatus,
 }
 
 // Display a section individually
