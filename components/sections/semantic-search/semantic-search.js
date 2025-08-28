aca427d0de29edd9db3d15b6b5d0836dd957c556
David Glymph <Woozl@users.noreply.github.com>
2025-08-27 23:23:00 -0400

    made search bar a form, check query params for initial state, mobile sizing

--- components/sections/semantic-search/semantic-search.js
+++ components/sections/semantic-search/semantic-search.js
@@ -1,11 +1,8 @@
-import { Button, IconButton, OutlinedInput, styled } from "@mui/material"
-import PageTitle from "../page-title"
-import { useMemo, useState } from "react"
-import { Results } from "./results"
-import { QueryCacheProvider } from "utils/use-query"
 import { Refresh } from "@mui/icons-material"
-import { useCallback } from "react"
+import { Button, IconButton, OutlinedInput, styled } from "@mui/material"
 import { useRouter } from "next/router"
+import { useCallback, useState } from "react"
+import { QueryCacheProvider } from "utils/use-query"
 
 const SUGGESTIONS = [
   "addiction treatment",
@@ -41,10 +38,18 @@ const SearchButton = styled(Button)(() => ({
   borderBottomLeftRadius: "0",
 }))
 
+function getQueryParam(param) {
+  if (typeof window === "undefined") return null
+  const urlParams = new URLSearchParams(window.location.search)
+  return urlParams.get(param)
+}
+
 export default function SemanticSearch({ data }) {
   const router = useRouter()
 
-  const [searchInputValue, setSearchInputValue] = useState("")
+  const [searchInputValue, setSearchInputValue] = useState(
+    getQueryParam(data.redirect_query_param) ?? ""
+  )
   const [selectedSuggestions, setSelectedSuggestions] = useState(
     getRandomSuggestions(3)
   )
@@ -62,11 +67,17 @@ export default function SemanticSearch({ data }) {
 
   return (
     <QueryCacheProvider>
-      <div className="bg-[#f4f1f5] w-full px-11 py-16 my-8">
+      <div className="bg-[#f4f1f5] w-full sm:py-16 py-10 sm:my-8">
         <div className="container text-[#8b3264] font-bold text-lg flex flex-col gap-4">
           <span className="text-xl">{data?.guide_text}</span>
 
-          <div className="flex">
+          <form
+            className="flex"
+            onSubmit={(e) => {
+              e.preventDefault()
+              searchTermHandler(searchInputValue)
+            }}
+          >
             <SearchBar
               id="search-bar"
               value={searchInputValue}
@@ -74,34 +85,32 @@ export default function SemanticSearch({ data }) {
                 setSearchInputValue(e.target.value)
               }}
             />
-            <SearchButton
-              variant="contained"
-              onClick={() => {
-                searchTermHandler(searchInputValue)
-              }}
-            >
+            <SearchButton variant="contained" type="submit">
               {data?.button_text}
             </SearchButton>
-          </div>
-          <div className="flex gap-2 items-center">
+          </form>
+          <div className="flex flex-col lg:flex-row gap-2 items-start lg:items-center max-w-full">
             <span>Example terms to search for:</span>
-            <ul className="flex gap-4 text-white">
-              {selectedSuggestions.map((term) => (
-                <Button
-                  variant="contained"
-                  key={term}
-                  onClick={() => {
-                    searchTermHandler(term)
-                    setSearchInputValue(term)
-                  }}
-                >
-                  {term}
-                </Button>
-              ))}
-            </ul>
-            <IconButton onClick={changeRandomSuggestions}>
-              <Refresh />
-            </IconButton>
+            <div className="flex gap-2 max-w-full overflow-auto">
+              <div className="flex gap-4 text-white">
+                {selectedSuggestions.map((term) => (
+                  <Button
+                    variant="contained"
+                    key={term}
+                    sx={{ whiteSpace: "nowrap" }}
+                    onClick={() => {
+                      searchTermHandler(term)
+                      setSearchInputValue(term)
+                    }}
+                  >
+                    {term}
+                  </Button>
+                ))}
+              </div>
+              <IconButton onClick={changeRandomSuggestions}>
+                <Refresh />
+              </IconButton>
+            </div>
           </div>
         </div>
       </div>
