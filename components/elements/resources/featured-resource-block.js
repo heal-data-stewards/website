595f4c04773a6c901788dc8c84b669132cdb59f8
suejinkim20 <kim.suejin.20@gmail.com>
2025-10-31 13:50:52 -0400

    Add tooltip to featured resource block component

--- components/elements/resources/featured-resource-block.js
+++ components/elements/resources/featured-resource-block.js
@@ -1,35 +1,55 @@
 import React from "react"
 import Link from "next/link"
 import Typography from "@mui/material/Typography"
+import Tooltip from "@mui/material/Tooltip"
 
 export function FeaturedResourceBlock({ data }) {
   return (
     <Link href={`/${data.url || "coming-soon"}`} passHref>
-      <div
-        className="featured-resource-block"
-        style={{
-          position: "relative",
-          backgroundImage: `url(${data.img.url})`,
-          clipPath:
-            "polygon(0px 0px, 100% 0px,100% calc(100%  - 48px),calc(100% - 48px) 100%, 0px 100%)",
-          backgroundSize: "cover",
-          cursor: "pointer",
-          height: "200px",
-          marginBottom: "2.5rem",
+      <Tooltip
+        title={data.blurb || ""}
+        placement="top-end"
+        arrow
+        componentsProps={{
+          tooltip: {
+            sx: {
+              fontSize: "1rem",
+              padding: "8px 12px",
+              bgcolor: "primary.main",
+              color: "primary.contrastText",
+              "& .MuiTooltip-arrow": {
+                color: "primary.main",
+              },
+            },
+          },
         }}
       >
-        <h1
-          className="text-white text-4xl p-10 resource-block-title"
+        <div
+          className="featured-resource-block"
           style={{
-            backgroundColor: "transparent",
-            position: "absolute",
-            zIndex: "2",
-            wordBreak: "break-word",
+            position: "relative",
+            backgroundImage: `url(${data.img.url})`,
+            clipPath:
+              "polygon(0px 0px, 100% 0px,100% calc(100%  - 48px),calc(100% - 48px) 100%, 0px 100%)",
+            backgroundSize: "cover",
+            cursor: "pointer",
+            height: "200px",
+            marginBottom: "2.5rem",
           }}
         >
-          {data.title}
-        </h1>
-      </div>
+          <h1
+            className="text-white text-4xl p-10 resource-block-title"
+            style={{
+              backgroundColor: "transparent",
+              position: "absolute",
+              zIndex: "2",
+              wordBreak: "break-word",
+            }}
+          >
+            {data.title}
+          </h1>
+        </div>
+      </Tooltip>
     </Link>
   )
 }
