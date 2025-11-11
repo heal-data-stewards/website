447fe93ce5545a3b75aea54f11010e2d1e61a5ee
suejinkim20 <kim.suejin.20@gmail.com>
2025-11-11 12:49:47 -0500

    move styles from index.css to resource block components

--- components/elements/resources/featured-resource-block.js
+++ components/elements/resources/featured-resource-block.js
@@ -1,55 +1,97 @@
 import React from "react"
 import Link from "next/link"
-import Typography from "@mui/material/Typography"
 import Tooltip from "@mui/material/Tooltip"
+import Markdown from "../../elements/markdown"
+import { styled } from "@mui/material/styles"
+
+const FeaturedBlock = styled("a")(({ img }) => ({
+  position: "relative",
+  backgroundImage: `url(${img})`,
+  backgroundSize: "cover",
+  cursor: "pointer",
+  height: "200px",
+  marginBottom: "2.5rem",
+  width: "calc((100% - 25px) / 2)",
+  overflow: "hidden",
+  display: "block",
+
+  "&:before": {
+    content: '""',
+    backgroundColor: "#982568",
+    opacity: 0.8,
+    display: "block",
+    height: "100%",
+    position: "absolute",
+    width: "100%",
+    transition: "opacity 0.25s ease",
+  },
+
+  "&:hover:before": {
+    backgroundColor: "#420F2C",
+    opacity: 0.9,
+  },
+
+  "&:after": {
+    content: '""',
+    position: "absolute",
+    bottom: 0,
+    right: -50,
+    width: 100,
+    height: 100,
+    background: "white",
+    transform: "rotate(45deg) translate(50px, 50px)",
+  },
+
+  "&:focus": {
+    border: "3px solid #0044B3",
+    outlineOffset: 3,
+  },
+}))
+
+const Title = styled("h1")({
+  position: "absolute",
+  zIndex: 2,
+  color: "white",
+  fontSize: "2.25rem",
+  padding: "2rem",
+  wordBreak: "break-word",
+  margin: 0,
+})
 
 export function FeaturedResourceBlock({ data }) {
   return (
-    <Link href={`/${data.url || "coming-soon"}`} passHref>
-      <Tooltip
-        title={data.blurb || ""}
-        placement="top-end"
-        arrow
-        componentsProps={{
-          tooltip: {
-            sx: {
-              fontSize: "1rem",
-              padding: "8px 12px",
-              bgcolor: "primary.main",
-              color: "primary.contrastText",
-              "& .MuiTooltip-arrow": {
-                color: "primary.main",
+    <Link href={`/${data.url || "coming-soon"}`} passHref legacyBehavior>
+      {data.blurb ? (
+        <Tooltip
+          placement="top-end"
+          arrow
+          disableInteractive
+          title={<Markdown>{data.blurb || ""}</Markdown>}
+          componentsProps={{
+            tooltip: {
+              sx: {
+                fontSize: "1rem",
+                padding: "8px 12px",
+                bgcolor: "primary.main",
+                color: "primary.contrastText",
+                "& .MuiTooltip-arrow": {
+                  color: "primary.main",
+                },
+                minWidth: "500px",
               },
             },
-          },
-        }}
-      >
-        <div
-          className="featured-resource-block"
-          style={{
-            position: "relative",
-            backgroundImage: `url(${data.img.url})`,
-            clipPath:
-              "polygon(0px 0px, 100% 0px,100% calc(100%  - 48px),calc(100% - 48px) 100%, 0px 100%)",
-            backgroundSize: "cover",
-            cursor: "pointer",
-            height: "200px",
-            marginBottom: "2.5rem",
           }}
         >
-          <h1
-            className="text-white text-4xl p-10 resource-block-title"
-            style={{
-              backgroundColor: "transparent",
-              position: "absolute",
-              zIndex: "2",
-              wordBreak: "break-word",
-            }}
-          >
-            {data.title}
-          </h1>
-        </div>
-      </Tooltip>
+          <FeaturedBlock aria-label={data.title} img={data.img.url}>
+            <Title>{data.title}</Title>
+          </FeaturedBlock>
+        </Tooltip>
+      ) : (
+        <FeaturedBlock aria-label={data.title} img={data.img.url}>
+          <Title>{data.title}</Title>
+        </FeaturedBlock>
+      )}
     </Link>
   )
 }
+export default FeaturedResourceBlock
--- components/elements/resources/resource-block.js
+++ components/elements/resources/resource-block.js
@@ -1,35 +1,99 @@
+// src/components/resources/resource-block.js
 import React from "react"
 import Link from "next/link"
+import Tooltip from "@mui/material/Tooltip"
 import Typography from "@mui/material/Typography"
+import { styled } from "@mui/material/styles"
+import Markdown from "../../elements/markdown"
+
+const Block = styled("a")(({ img }) => ({
+  width: "calc((100% - 50px) / 3)",
+  position: "relative",
+  overflow: "hidden",
+  height: 300,
+  marginBottom: "2.5rem",
+  cursor: "pointer",
+  display: "block",
+  backgroundImage: `url(${img})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+
+  "&:before": {
+    content: '""',
+    backgroundColor: "#532565",
+    opacity: 0.8,
+    display: "block",
+    height: "100%",
+    position: "absolute",
+    width: "100%",
+    transition: "opacity 0.25s ease",
+  },
+
+  "&:hover:before": {
+    backgroundColor: "#250033",
+    opacity: 0.9,
+  },
+
+  "&:after": {
+    content: '""',
+    position: "absolute",
+    bottom: 0,
+    right: -50,
+    width: 100,
+    height: 100,
+    background: "white",
+    transform: "rotate(45deg) translate(50px, 50px)",
+  },
+
+  "&:focus": {
+    outline: "3px solid #0044B3",
+    outlineOffset: 3,
+  },
+}))
+
+const Title = styled("h1")({
+  position: "absolute",
+  zIndex: 2,
+  color: "white",
+  fontSize: "2.25rem",
+  padding: "2rem",
+  wordBreak: "break-word",
+  margin: 0,
+})
 
 export function ResourceBlock({ data }) {
   return (
-    <Link href={`/${data.url || "coming-soon"}`} passHref>
-      <div
-        className="resource-block"
-        style={{
-          position: "relative",
-          backgroundImage: `url(${data.img.url})`,
-          clipPath:
-            "polygon(0px 0px, 100% 0px,100% calc(100%  - 48px),calc(100% - 48px) 100%, 0px 100%)",
-          backgroundSize: "cover",
-          cursor: "pointer",
-          height: "300px",
-          marginBottom: "2.5rem",
-        }}
-      >
-        <h1
-          className="text-white text-4xl p-10 resource-block-title"
-          style={{
-            backgroundColor: "transparent",
-            position: "absolute",
-            zIndex: "2",
-            wordBreak: "break-word",
+    <Link href={`/${data.url || "coming-soon"}`} passHref legacyBehavior>
+      {data.tooltip ? (
+        <Tooltip
+          placement="top"
+          arrow
+          disableInteractive
+          title={<Markdown>{data.tooltip}</Markdown>}
+          componentsProps={{
+            tooltip: {
+              sx: {
+                fontSize: "1rem",
+                padding: "8px 12px",
+                bgcolor: "primary.main",
+                color: "primary.contrastText",
+                "& .MuiTooltip-arrow": {
+                  color: "primary.main",
+                },
+                minWidth: "500px",
+              },
+            },
           }}
         >
-          {data.title}
-        </h1>
-      </div>
+          <Block img={data.img.url} aria-label={data.title}>
+            <Title>{data.title}</Title>
+          </Block>
+        </Tooltip>
+      ) : (
+        <Block img={data.img.url} aria-label={data.title}>
+          <Title>{data.title}</Title>
+        </Block>
+      )}
     </Link>
   )
 }
