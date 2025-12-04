8aa5c8cb25c9fa3e14204eb8218389eef5951c8f
suejinkim20 <kim.suejin.20@gmail.com>
2025-12-04 16:26:15 -0500

    move google analytics script to _app.js and use Next Script component

--- pages/_document.js
+++ pages/_document.js
@@ -6,24 +6,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
