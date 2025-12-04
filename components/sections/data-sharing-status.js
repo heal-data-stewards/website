04eab71a3100c3fc3c5e86c2e404a9cb21557788
David Glymph <Woozl@users.noreply.github.com>
2025-12-04 08:32:07 -0500

    move platform id helper tooltip to /road-map page as well

--- components/sections/data-sharing-status.js
+++ components/sections/data-sharing-status.js
@@ -3,6 +3,9 @@ import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
 import Link from "next/link"
+import Markdown from "../elements/markdown"
+import { styled, Tooltip, tooltipClasses } from "@mui/material"
+import InfoIcon from "@mui/icons-material/Info"
 
 export default function DataSharingStatus({ data }) {
   const [value, setValue] = React.useState("")
@@ -13,7 +16,7 @@ export default function DataSharingStatus({ data }) {
 
   return (
     <div className={"container mb-16"}>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
         <TextField
           id="outlined-basic"
           label={data.inputBoxLabel}
@@ -24,7 +27,6 @@ export default function DataSharingStatus({ data }) {
         />
         <Button
           style={{
-            marginLeft: "2rem",
             height: "3.5rem",
             fontSize: "1rem",
             padding: "1rem 2rem",
@@ -41,7 +43,35 @@ export default function DataSharingStatus({ data }) {
             <a>{data.buttonText}</a>
           </Link>
         </Button>
+        <LightTooltip
+          title={
+            <Markdown>
+              To locate your HEAL Data Platform ID: 1) search for your study on
+              the [HEAL Data Platform Discovery
+              page](https://healdata.org/portal/discovery), then 2) click on the
+              Study Name in the search results to open the study page on the
+              right; the HDP ID will be in gray letters under the study title
+              (ex: HDP00258). If you have your study’s permalink, your HDP ID
+              will be the number at the end of the URL. If you need assistance,
+              contact the HEAL Platform Team at
+              [heal-support@gen3.org](mailto:heal-support@gen3.org).
+            </Markdown>
+          }
+        >
+          <InfoIcon sx={{ color: "#cacaca" }} />
+        </LightTooltip>
       </Box>
     </div>
   )
 }
+
+const LightTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "white",
+    color: "rgba(0, 0, 0, 0.87)",
+    boxShadow: "0px 2px 4px 0px rgba(0, 0, 0, 0.3)",
+    fontSize: 8,
+  },
+}))
