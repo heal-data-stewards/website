96f3abf60351faa7a30068a74b808ce10a9b01be
suejinkim20 <kim.suejin.20@gmail.com>
2025-12-09 16:32:57 -0500

    Refactor FeaturedResourceBlock component to improve styling and remove Tooltip integration

--- components/elements/resources/featured-resource-block.js
+++ components/elements/resources/featured-resource-block.js
@@ -1,97 +1,102 @@
 import React from "react"
 import Link from "next/link"
-import Tooltip from "@mui/material/Tooltip"
 import Markdown from "../../elements/markdown"
 import { styled } from "@mui/material/styles"
 
-const FeaturedBlock = styled("a")(({ img }) => ({
+const Card = styled("a")(({ theme }) => ({
   position: "relative",
-  backgroundImage: `url(${img})`,
-  backgroundSize: "cover",
-  cursor: "pointer",
-  height: "200px",
-  marginBottom: "2.5rem",
-  width: "calc((100% - 25px) / 2)",
-  overflow: "hidden",
   display: "block",
-
-  "&:before": {
-    content: '""',
-    backgroundColor: "#982568",
-    opacity: 0.8,
-    display: "block",
-    height: "100%",
-    position: "absolute",
+  height: 250,
+  width: "calc((100% - 25px) / 2)",
+  marginBottom: "2.5rem",
+  cursor: "pointer",
+  clipPath:
+    "polygon(0px 0px, 100% 0px,100% calc(100%  - 48px),calc(100% - 48px) 100%, 0px 100%)",
+  overflow: "hidden",
+  textDecoration: "none",
+  color: "white",
+  [theme.breakpoints.down(1024)]: {
     width: "100%",
-    transition: "opacity 0.25s ease",
   },
-
-  "&:hover:before": {
-    backgroundColor: "#420F2C",
-    opacity: 0.9,
-  },
-
-  "&:after": {
-    content: '""',
-    position: "absolute",
-    bottom: 0,
-    right: -50,
-    width: 100,
-    height: 100,
-    background: "white",
-    transform: "rotate(45deg) translate(50px, 50px)",
-  },
-
-  "&:focus": {
-    border: "3px solid #0044B3",
-    outlineOffset: 3,
+  "&:focus-visible": {
+    outline: "2px solid #ffffff",
+    outlineOffset: 4,
   },
 }))
 
-const Title = styled("h1")({
+const VisualLayer = styled("div")(({ img }) => ({
   position: "absolute",
-  zIndex: 2,
-  color: "white",
-  fontSize: "2.25rem",
-  padding: "2rem",
-  wordBreak: "break-word",
-  margin: 0,
+  inset: 0,
+  backgroundImage: `url(${img})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  zIndex: 1,
+  transition: "opacity 0.3s ease",
+  "&::before": {
+    content: '""',
+    position: "absolute",
+    inset: 0,
+    backgroundColor: "#982568",
+    opacity: 0.8,
+    transition: "opacity 0.3s ease, background-color 0.3s ease",
+  },
+}))
+
+const PreviewLayer = styled("div")({
+  position: "absolute",
+  inset: 0,
+  backgroundColor: "#420F2C",
+  padding: "1rem",
+  overflowY: "auto",
+  zIndex: 0,
+  "& h3": {
+    marginTop: 0,
+    fontWeight: 600,
+    fontSize: "1.5rem",
+  },
 })
 
+const Title = styled("h3")({
+  position: "absolute",
+  top: "1rem",
+  left: "1rem",
+  margin: 0,
+  paddingRight: "1rem",
+  fontSize: "2rem",
+  zIndex: 2,
+  wordBreak: "break-word",
+})
+
+const CardContainer = styled(Card)(({ hasBlurb }) => ({
+  "&:hover .visual, &:focus .visual": hasBlurb
+    ? { opacity: 0 } // fade out image
+    : {
+        "&::before": {
+          backgroundColor: "#420F2C",
+          opacity: 0.9, // darken hover cue
+        },
+      },
+}))
+
 export function FeaturedResourceBlock({ data }) {
+  const hasBlurb = Boolean(data.blurb)
+  const href = `/${data.url || "coming-soon"}`
+
   return (
-    <Link href={`/${data.url || "coming-soon"}`} passHref legacyBehavior>
-      {data.blurb ? (
-        <Tooltip
-          placement="top-end"
-          arrow
-          disableInteractive
-          title={<Markdown>{data.blurb || ""}</Markdown>}
-          componentsProps={{
-            tooltip: {
-              sx: {
-                fontSize: "1rem",
-                padding: "8px 12px",
-                bgcolor: "primary.main",
-                color: "primary.contrastText",
-                "& .MuiTooltip-arrow": {
-                  color: "primary.main",
-                },
-                minWidth: "500px",
-              },
-            },
-          }}
-        >
-          <FeaturedBlock aria-label={data.title} img={data.img.url}>
-            <Title>{data.title}</Title>
-          </FeaturedBlock>
-        </Tooltip>
-      ) : (
-        <FeaturedBlock aria-label={data.title} img={data.img.url}>
-          <Title>{data.title}</Title>
-        </FeaturedBlock>
-      )}
+    <Link href={href} passHref legacyBehavior>
+      <CardContainer aria-label={data.title} hasBlurb={hasBlurb}>
+        {hasBlurb && (
+          <PreviewLayer>
+            <h3>{data.title}</h3>
+            <Markdown>{data.blurb}</Markdown>
+          </PreviewLayer>
+        )}
+
+        <VisualLayer img={data.img.url} className="visual" />
+        <Title>{data.title}</Title>
+      </CardContainer>
     </Link>
   )
 }
+
 export default FeaturedResourceBlock
