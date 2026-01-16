230eba0ba306cd394983696af9cdf27c29977394
suejinkim20 <kim.suejin.20@gmail.com>
2026-01-16 12:22:01 -0500

    Include collection snapshot in download tracking

--- components/sections/semantic-search/analytics/collectionDownload.js
+++ components/sections/semantic-search/analytics/collectionDownload.js
@@ -1,6 +1,6 @@
 import { sendCustomEvent } from "utils/analytics"
 
-function stringifyArray(value) {
+function stringify(value) {
   return JSON.stringify(value)
 }
 
@@ -10,21 +10,30 @@ export function trackCollectionDownloadClick({
   concepts,
   variables,
 }) {
+  const collectionSnapshot = {
+    studies,
+    cdes,
+    concepts,
+    variables,
+  }
+
   sendCustomEvent("hss_collection_downloaded", {
-    downloaded_collection_study_ids: stringifyArray(studies.ids),
-    downloaded_collection_study_labels: stringifyArray(studies.labels),
+    downloaded_collection_study_ids: stringify(studies.ids),
+    downloaded_collection_study_labels: stringify(studies.labels),
     downloaded_collection_study_count: studies.count,
 
-    downloaded_collection_cde_ids: stringifyArray(cdes.ids),
-    downloaded_collection_cde_labels: stringifyArray(cdes.labels),
+    downloaded_collection_cde_ids: stringify(cdes.ids),
+    downloaded_collection_cde_labels: stringify(cdes.labels),
     downloaded_collection_cde_count: cdes.count,
 
-    downloaded_collection_concept_ids: stringifyArray(concepts.ids),
-    downloaded_collection_concept_labels: stringifyArray(concepts.labels),
+    downloaded_collection_concept_ids: stringify(concepts.ids),
+    downloaded_collection_concept_labels: stringify(concepts.labels),
     downloaded_collection_concept_count: concepts.count,
 
-    downloaded_collection_variable_ids: stringifyArray(variables.ids),
-    downloaded_collection_variable_labels: stringifyArray(variables.labels),
+    downloaded_collection_variable_ids: stringify(variables.ids),
+    downloaded_collection_variable_labels: stringify(variables.labels),
     downloaded_collection_variable_count: variables.count,
+
+    downloaded_collection_snapshot: stringify(collectionSnapshot),
   })
 }
